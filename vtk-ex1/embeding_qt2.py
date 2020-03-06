be2e1a01ba025c85f90bced79068bb00fb6fb667
Taku Nakai <t.nakai.office@gmail.com>
2020-03-07 08:04:16 +0900

    embed qt in VTK

---
+++ vtk-ex1/embeding_qt2.py
@@ -0,0 +1,53 @@
+# Contributed by Eric E Monson
+
+from PyQt5.QtWidgets import QMainWindow, QApplication
+from PyQt5.QtWidgets import QGridLayout, QWidget
+import vtk
+from vtk.qt.QVTKRenderWindowInteractor import QVTKRenderWindowInteractor
+import sys
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(603, 553)
+        self.centralWidget = QWidget(MainWindow)
+        self.gridlayout = QGridLayout(self.centralWidget)
+        self.vtkWidget = QVTKRenderWindowInteractor(self.centralWidget)
+        self.gridlayout.addWidget(self.vtkWidget, 0, 0, 1, 1)
+        MainWindow.setCentralWidget(self.centralWidget)
+
+
+class SimpleView(QMainWindow):
+
+    def __init__(self, parent=None):
+        QMainWindow.__init__(self, parent)
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+        self.ren = vtk.vtkRenderer()
+        self.ui.vtkWidget.GetRenderWindow().AddRenderer(self.ren)
+        self.iren = self.ui.vtkWidget.GetRenderWindow().GetInteractor()
+
+        # Create source
+        source = vtk.vtkSphereSource()
+        source.SetCenter(0, 0, 0)
+        source.SetRadius(5.0)
+
+        # Create a mapper
+        mapper = vtk.vtkPolyDataMapper()
+        mapper.SetInputConnection(source.GetOutputPort())
+
+        # Create an actor
+        actor = vtk.vtkActor()
+        actor.SetMapper(mapper)
+
+        self.ren.AddActor(actor)
+
+
+if __name__ == "__main__":
+
+    app = QApplication(sys.argv)
+    window = SimpleView()
+    window.show()
+    window.iren.Initialize()  # Need this line to actually show the render inside Qt
+    sys.exit(app.exec_())
