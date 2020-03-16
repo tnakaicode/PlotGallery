e116af894eb21e53a0dcdcdb1d4a0a379d9a1312
Taku Nakai <tnakaicode@gmail.com>
2020-03-16 16:16:02 +0900

    format

--- occ_gallery/core_geometry_surface_from_curves.py
+++ occ_gallery/core_geometry_surface_from_curves.py
@@ -1,21 +1,21 @@
 #!/usr/bin/env python
 
-##Copyright 2009-2015 Jelle Feringa (jelleferinga@gmail.com)
+# Copyright 2009-2015 Jelle Feringa (jelleferinga@gmail.com)
 ##
-##This file is part of pythonOCC.
+# This file is part of pythonOCC.
 ##
-##pythonOCC is free software: you can redistribute it and/or modify
-##it under the terms of the GNU Lesser General Public License as published by
-##the Free Software Foundation, either version 3 of the License, or
-##(at your option) any later version.
+# pythonOCC is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
 ##
-##pythonOCC is distributed in the hope that it will be useful,
-##but WITHOUT ANY WARRANTY; without even the implied warranty of
-##MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-##GNU Lesser General Public License for more details.
+# pythonOCC is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
 ##
-##You should have received a copy of the GNU Lesser General Public License
-##along with pythonOCC.  If not, see <http://www.gnu.org/licenses/>.
+# You should have received a copy of the GNU Lesser General Public License
+# along with pythonOCC.  If not, see <http://www.gnu.org/licenses/>.
 
 from __future__ import print_function
 
@@ -80,10 +80,11 @@ def surface_from_curves():
     aBSplineSurface1 = aGeomFill1.Surface()
     aBSplineSurface2 = aGeomFill2.Surface()
     aBSplineSurface3 = aGeomFill3.Surface()
-   
+
     display.DisplayShape(make_face(aBSplineSurface1, 1e-6))
     display.DisplayShape(make_face(aBSplineSurface2, 1e-6))
     display.DisplayShape(make_face(aBSplineSurface3, 1e-6), update=True)
+
 
 if __name__ == '__main__':
     surface_from_curves()
