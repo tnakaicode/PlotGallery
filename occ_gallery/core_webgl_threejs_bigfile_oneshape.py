82c1ca80e5648ed1ebc8a0c85bbcde500c19c0a2
Taku Nakai <tnakaicode@gmail.com>
2020-03-27 17:05:53 +0900

    format webgl

--- occ_gallery/core_webgl_threejs_bigfile_oneshape.py
+++ occ_gallery/core_webgl_threejs_bigfile_oneshape.py
@@ -1,21 +1,21 @@
 #!/usr/bin/env python
 
-##Copyright 2009-2016 Thomas Paviot (tpaviot@gmail.com)
+# Copyright 2009-2016 Thomas Paviot (tpaviot@gmail.com)
 ##
-##This file is part of pythonOCC.
+# This file is part of pythonOCC.
 ##
-##pythonOCC is free software: you can redistribute it and/or modify
-##it under the terms of the GNU Lesser General Public License as published by
-##the Free Software Foundation, either version 3 of the License, or
-##(at your option) any later version.
+# pythonOCC is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
 ##
-##pythonOCC is distributed in the hope that it will be useful,
-##but WITHOUT ANY WARRANTY; without even the implied warranty of
-##MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-##GNU Lesser General Public License for more details.
+# pythonOCC is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
 ##
-##You should have received a copy of the GNU Lesser General Public License
-##along with pythonOCC.  If not, see <http://www.gnu.org/licenses/>.
+# You should have received a copy of the GNU Lesser General Public License
+# along with pythonOCC.  If not, see <http://www.gnu.org/licenses/>.
 
 import os
 
@@ -24,7 +24,8 @@ from OCC.Display.WebGl import threejs_renderer
 
 # opens a big step file
 # render the whole assembly as a single shape
-big_shp = read_step_file(os.path.join('..', 'assets', 'models', 'RC_Buggy_2_front_suspension.stp'))
+big_shp = read_step_file(os.path.join(
+    'models', 'RC_Buggy_2_front_suspension.stp'))
 
 my_renderer = threejs_renderer.ThreejsRenderer()
 my_renderer.DisplayShape(big_shp)
