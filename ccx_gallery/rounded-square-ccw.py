6e61162e25df2f13e858c2f72c0892c6ec2bd057
Taku Nakai <t.nakai.office@gmail.com>
2020-04-03 21:17:18 +0900

    rounded sqaure

--- ccx_gallery/rounded-square-ccw.py
+++ ccx_gallery/rounded-square-ccw.py
@@ -24,11 +24,11 @@ axial = 10
 
 part = pyc.Part(model)
 part.goto(radius_inner, axial)
-part.draw_line_ax(length*0.5)
-part.draw_line_ax(length*0.5)
+part.draw_line_ax(length * 0.5)
+part.draw_line_ax(length * 0.5)
 line_1 = part.draw_line_rad(thickness)[0]
 line_2 = part.draw_line_ax(-length)[0]
-part.draw_line_rad(-thickness*0.4)
+part.draw_line_rad(-thickness * 0.4)
 part.draw_line_to(radius_inner, axial)
 model.plot_geometry(model_name + '_pre', display=show_gui)
 part.fillet_lines(line_1, line_2, radius)
@@ -41,6 +41,6 @@ model.view.print_summary()
 model.set_etype('axisym', part)
 model.set_eshape(eshape, 2)
 model.mesh(1.0, 'gmsh')
-model.plot_elements(model_name+'_elements', display=show_gui)
+model.plot_elements(model_name + '_elements', display=show_gui)
 model.view.print_summary()
 model.print_summary()
