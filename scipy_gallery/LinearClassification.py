98081e3824756338ff1c943fe09bc5d6cd1ab0cc
tnakaicode <tnakaicode@gmail.com>
2020-10-17 20:26:32 +0900

    format

--- scipy_gallery/LinearClassification.py
+++ scipy_gallery/LinearClassification.py
@@ -3,10 +3,10 @@
 
 # Linear classification
 # ======================================================================
-# 
+#
 # Fisher's Linear Discriminant
 # ----------------------------
-# 
+#
 # The first example shows the implementation of Fisher's Linear Classifier
 # for 2-class problem and this algorithm is precisely described in book
 # "Pattern Recognition and Machine Learning" by Christopher M Bishop (p
@@ -22,7 +22,7 @@
 # with other instances. Iris data set is available here:
 # <http://archive.ics.uci.edu/ml/datasets/Iris/> or here (comma separated
 # format) - [bezdekIris.data.txt](files/attachments/LinearClassification/bezdekIris.data.txt)
-#  
+#
 
 # In[ ]:
 
@@ -32,49 +32,48 @@ from __future__ import division
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 def read_data():
-  f=open("Iris.txt", 'r')
-  lines=[line.strip() for line in f.readlines()]
-  f.close()
+    f = open("Iris.txt", 'r')
+    lines = [line.strip() for line in f.readlines()]
+    f.close()
 
-  lines=[line.split(",") for line in lines if line]
+    lines = [line.split(",") for line in lines if line]
 
-  class1=np.array([line[:4] for line in lines if line[-1]=="Iris-setosa"], dtype=np.float)
+    class1 = np.array([line[:4] for line in lines if line[-1]
+                       == "Iris-setosa"], dtype=np.float)
 
-  class2=np.array([line[:4] for line in lines if line[-1]!="Iris-setosa"], dtype=np.float)
-  
-  return class1, class2
- 
-  
-def main():
+    class2 = np.array([line[:4] for line in lines if line[-1]
+                       != "Iris-setosa"], dtype=np.float)
 
-  class1, class2=read_data()
+    return class1, class2
 
-  mean1=np.mean(class1, axis=0)
-  mean2=np.mean(class2, axis=0)
-  
-  #calculate variance within class
-  Sw=np.dot((class1-mean1).T, (class1-mean1))+np.dot((class2-mean2).T, (class2-mean2))
-  
-  #calculate weights which maximize linear separation
-  w=np.dot(np.linalg.inv(Sw), (mean2-mean1))
 
-  print "vector of max weights", w
-  #projection of classes on 1D space
-  plt.plot(np.dot(class1, w), [0]*class1.shape[0], "bo", label="Iris-setosa")
-  plt.plot(np.dot(class2, w), [0]*class2.shape[0], "go", label="Iris-versicolor and Iris-virginica")
-  plt.legend()
-  
-  plt.show()
+class1, class2 = read_data()
+mean1 = np.mean(class1, axis=0)
+mean2 = np.mean(class2, axis=0)
 
-main()
+# calculate variance within class
+Sw = np.dot((class1 - mean1).T, (class1 - mean1)) + \
+    np.dot((class2 - mean2).T, (class2 - mean2))
+
+# calculate weights which maximize linear separation
+w = np.dot(np.linalg.inv(Sw), (mean2 - mean1))
+print("vector of max weights", w)
+# projection of classes on 1D space
+plt.plot(np.dot(class1, w), [0] * class1.shape[0], "bo", label="Iris-setosa")
+plt.plot(np.dot(class2, w), [0] * class2.shape[0],
+         "go", label="Iris-versicolor and Iris-virginica")
+plt.legend()
+
+plt.show()
 
 
 # ![](files/attachments/LinearClassification/Fisher_disrciminant.JPG)
-# 
+#
 # Probabilistic Generative Model
 # ------------------------------
-# 
+#
 # This program is the implementation of Probabilistic Generative Model for
 # K-class problem which is also described in book "Pattern Recognition and
 # Machine Learning" by Christopher M Bishop (p 196, Section 4.2). We try
@@ -95,100 +94,117 @@ import numpy as np
 import matplotlib.pyplot as plt
 import math
 
+
 def read_data():
-  f=open("Iris.txt", 'r')
-  lines=[line.strip() for line in f.readlines()]
-  f.close()
+    f = open("Iris.txt", 'r')
+    lines = [line.strip() for line in f.readlines()]
+    f.close()
 
-  lines=[line.split(",") for line in lines if line]
+    lines = [line.split(",") for line in lines if line]
 
-  data=np.array([line[:4] for line in lines if line], dtype=np.float) 
+    data = np.array([line[:4] for line in lines if line], dtype=np.float)
 
-  class1=np.array([line[:4] for line in lines if line[-1]=="Iris-setosa"], dtype=np.float)
+    class1 = np.array([line[:4] for line in lines if line[-1]
+                       == "Iris-setosa"], dtype=np.float)
 
-  class2=np.array([line[:4] for line in lines if line[-1]=="Iris-virginica"], dtype=np.float)
+    class2 = np.array([line[:4] for line in lines if line[-1]
+                       == "Iris-virginica"], dtype=np.float)
 
-  class3=np.array([line[:4] for line in lines if line[-1]=="Iris-versicolor"], dtype=np.float)
+    class3 = np.array([line[:4] for line in lines if line[-1]
+                       == "Iris-versicolor"], dtype=np.float)
 
- #list of class labels
-  labels=[]
-  for line in lines:
-   strt=line.pop()
-   labels.append(strt)
-  #create array of labels
-  labels=[line.split(",") for line in labels if line]
-  t=np.zeros(shape=(150, 3))
-  #create target vector encoded according to 1-of-K scheme
-  for i in xrange(len(data)):
-   if labels[i]==["Iris-setosa"]: t[i][0]=1
-   elif labels[i]==["Iris-versicolor"]: t[i][1]=1
-   elif labels[i]==["Iris-virginica"]: t[i][2]=1
-  
-  return class1, class2, class3, data, t
+   # list of class labels
+    labels = []
+    for line in lines:
+        strt = line.pop()
+        labels.append(strt)
+    # create array of labels
+    labels = [line.split(",") for line in labels if line]
+    t = np.zeros(shape=(150, 3))
+    # create target vector encoded according to 1-of-K scheme
+    for i in range(len(data)):
+        if labels[i] == ["Iris-setosa"]:
+            t[i][0] = 1
+        elif labels[i] == ["Iris-versicolor"]:
+            t[i][1] = 1
+        elif labels[i] == ["Iris-virginica"]:
+            t[i][2] = 1
+
+    return class1, class2, class3, data, t
+
 
 def gaussian(x, mean, cov):
-  xm=np.reshape((x-mean), (-1, 1))
-  px=1/(math.pow(2.0*math.pi, 2))*1/math.sqrt(np.linalg.det(cov))*math.exp(-(np.dot(np.dot(xm.T, np.linalg.inv(cov)), xm))/2)
-  return px
-
-def main():
- class1, class2, class3, data, t=read_data()
- 
- count=np.zeros(shape=(150,1))
- t_assigned=np.zeros(shape=(150, 3))
- cov=np.zeros(shape=(3, 4, 4))
- mean=np.zeros(shape=(3, 4))
-
-  #compute means for each class
- mean1=class1.mean(axis=0)
- mean2=class2.mean(axis=0)
- mean3=class3.mean(axis=0)
-  #compute covariance matrices, such that the columns are variables and rows are observations of variables
- cov1=np.cov(class1, rowvar=0)
- cov2=np.cov(class2, rowvar=0)
- cov3=np.cov(class3, rowvar=0)
-  
-#compute gaussian likelihood functions p(x|Ck) for each class
- for i in xrange(len(data)):
-     px1=(1/3.0)*gaussian(data[i], mean1, cov1)
-     px2=(1/3.0)*gaussian(data[i], mean2, cov2)
-     px3=(1/3.0)*gaussian(data[i], mean3, cov3)
-     m=np.max([px1, px2, px3])
- #compute posterior probability p(Ck|x) assuming that p(x|Ck) is gaussian and the entire expression is wrapped by sigmoid function 
-     pc1=((math.exp(px1)*math.exp(-m))*math.exp(m))/((math.exp(px2)*math.exp(-m)+math.exp(px3)*math.exp(-m))*math.exp(m))
-     pc2=((math.exp(px2)*math.exp(-m))*math.exp(m))/((math.exp(px1)*math.exp(-m)+math.exp(px3)*math.exp(-m))*math.exp(m))
-     pc3=((math.exp(px3)*math.exp(-m))*math.exp(m))/((math.exp(px1)*math.exp(-m)+math.exp(px2)*math.exp(-m))*math.exp(m))
- #assign p(Ck|x)=1 if p(Ck|x)>>p(Cj|x) for all j!=k
-     if pc1>pc2 and pc1>pc3: t_assigned[i][0]=1
-     elif pc3>pc1 and pc3>pc2: t_assigned[i][1]=1
-     elif pc2>pc1 and pc2>pc3: t_assigned[i][2]=1
- #count the number of misclassifications
-     for j in xrange(3):
-      if t[i][j]-t_assigned[i][j]!=0: count[i]=1
-    
- cov=[cov1, cov2, cov3]
- mean=[mean1, mean2, mean3]
- 
- t1=np.zeros(shape=(len(class1), 1))
- t2=np.zeros(shape=(len(class2), 1))
- t3=np.zeros(shape=(len(class3), 1))
- for i in xrange(len(data)):
-  for j in xrange(len(class1)):
-   if t_assigned[i][0]==1: t1[j]=1
-   elif t_assigned[i][1]==1: t2[j]=2
-   elif t_assigned[i][2]==1: t3[j]=3
- 
- plt.plot(t1, "bo", label="Iris-setosa")
- plt.plot(t2, "go", label="Iris-versicolor")
- plt.plot(t3, "ro", label="Iris-virginica")
- plt.legend()
- plt.show()
-   
- print "number of misclassifications", sum(count), "assigned labels to data points", t_assigned, "target data", t
-
-main()
+    xm = np.reshape((x - mean), (-1, 1))
+    px = 1 / (math.pow(2.0 * math.pi, 2)) * 1 / math.sqrt(np.linalg.det(cov)
+                                                          ) * math.exp(-(np.dot(np.dot(xm.T, np.linalg.inv(cov)), xm)) / 2)
+    return px
 
 
-# This program resulted in the number of misclassifications = 3 out of all 150 instances 
-# 
+class1, class2, class3, data, t = read_data()
+
+count = np.zeros(shape=(150, 1))
+t_assigned = np.zeros(shape=(150, 3))
+cov = np.zeros(shape=(3, 4, 4))
+mean = np.zeros(shape=(3, 4))
+# compute means for each class
+mean1 = class1.mean(axis=0)
+mean2 = class2.mean(axis=0)
+mean3 = class3.mean(axis=0)
+# compute covariance matrices, such that the columns are variables and rows are observations of variables
+cov1 = np.cov(class1, rowvar=0)
+cov2 = np.cov(class2, rowvar=0)
+cov3 = np.cov(class3, rowvar=0)
+
+# compute gaussian likelihood functions p(x|Ck) for each class
+for i in range(len(data)):
+    px1 = (1 / 3.0) * gaussian(data[i], mean1, cov1)
+    px2 = (1 / 3.0) * gaussian(data[i], mean2, cov2)
+    px3 = (1 / 3.0) * gaussian(data[i], mean3, cov3)
+    m = np.max([px1, px2, px3])
+# compute posterior probability p(Ck|x) assuming that p(x|Ck) is gaussian and the entire expression is wrapped by sigmoid function
+    pc1 = ((math.exp(px1) * math.exp(-m)) * math.exp(m)) / ((math.exp(px2)
+                                                             * math.exp(-m) + math.exp(px3) * math.exp(-m)) * math.exp(m))
+    pc2 = ((math.exp(px2) * math.exp(-m)) * math.exp(m)) / ((math.exp(px1)
+                                                             * math.exp(-m) + math.exp(px3) * math.exp(-m)) * math.exp(m))
+    pc3 = ((math.exp(px3) * math.exp(-m)) * math.exp(m)) / ((math.exp(px1)
+                                                             * math.exp(-m) + math.exp(px2) * math.exp(-m)) * math.exp(m))
+# assign p(Ck|x)=1 if p(Ck|x)>>p(Cj|x) for all j!=k
+    if pc1 > pc2 and pc1 > pc3:
+        t_assigned[i][0] = 1
+    elif pc3 > pc1 and pc3 > pc2:
+        t_assigned[i][1] = 1
+    elif pc2 > pc1 and pc2 > pc3:
+        t_assigned[i][2] = 1
+# count the number of misclassifications
+    for j in range(3):
+        if t[i][j] - t_assigned[i][j] != 0:
+            count[i] = 1
+
+cov = [cov1, cov2, cov3]
+mean = [mean1, mean2, mean3]
+
+t1 = np.zeros(shape=(len(class1), 1))
+t2 = np.zeros(shape=(len(class2), 1))
+t3 = np.zeros(shape=(len(class3), 1))
+for i in range(len(data)):
+    for j in range(len(class1)):
+        if t_assigned[i][0] == 1:
+            t1[j] = 1
+        elif t_assigned[i][1] == 1:
+            t2[j] = 2
+        elif t_assigned[i][2] == 1:
+            t3[j] = 3
+
+plt.plot(t1, "bo", label="Iris-setosa")
+plt.plot(t2, "go", label="Iris-versicolor")
+plt.plot(t3, "ro", label="Iris-virginica")
+plt.legend()
+plt.show()
+
+print("number of misclassifications", sum(count),
+      "assigned labels to data points", t_assigned, "target data", t)
+
+
+# This program resulted in the number of misclassifications = 3 out of all 150 instances
+#
 # ![](files/attachments/LinearClassification/Probabilistic_model.PNG)
--- scipy_gallery/LinearRegression.py
+++ scipy_gallery/LinearRegression.py
@@ -3,31 +3,21 @@
 
 # Linear regression
 # ======================================================================
-# 
+#
 # Linear Regression Example
-
-# In[5]:
-
-
-# IPython magic to plot interactively on the notebook
-get_ipython().run_line_magic('matplotlib', 'notebook')
-
-
-# In[7]:
-
 
 from scipy import linspace, polyval, polyfit, sqrt, stats, randn
 from matplotlib.pyplot import plot, title, show, legend
 
 # Linear regression example
-# This is a very simple example of using two scipy tools 
+# This is a very simple example of using two scipy tools
 # for linear regression, polyfit and stats.linregress
 
 # Sample data creation
 
-# number of points 
+# number of points
 n = 50
-t = linspace(-5,5,n)
+t = linspace(-5, 5, n)
 # parameters
 a = 0.8
 b = -4
@@ -39,33 +29,29 @@ xn = x + randn(n)
 (ar, br) = polyfit(t, xn, 1)
 xr = polyval([ar, br], t)
 # compute the mean square error
-err = sqrt(sum((xr - xn)**2)/n)
+err = sqrt(sum((xr - xn)**2) / n)
 print('Linear regression using polyfit')
-print('parameters: a=%.2f b=%.2f \nregression: a=%.2f b=%.2f, ms error= %.3f' % (a, b, ar, br, err))
+print('parameters: a=%.2f b=%.2f \nregression: a=%.2f b=%.2f, ms error= %.3f' %
+      (a, b, ar, br, err))
 print('\n')
 
 
 # Linear regression using stats.linregress
 (a_s, b_s, r, tt, stderr) = stats.linregress(t, xn)
 print('Linear regression using stats.linregress')
-print('parameters: a=%.2f b=%.2f \nregression: a=%.2f b=%.2f, std error= %.3f' % (a,b,a_s,b_s,stderr))
+print('parameters: a=%.2f b=%.2f \nregression: a=%.2f b=%.2f, std error= %.3f' %
+      (a, b, a_s, b_s, stderr))
 print('\n')
 
 # matplotlib ploting
 title('Linear Regression Example')
-plot(t, x,'g.--')
+plot(t, x, 'g.--')
 plot(t, xn, 'k.')
 plot(t, xr, 'r.-')
-legend(['original','plus noise', 'regression'])
-show();
+legend(['original', 'plus noise', 'regression'])
+show()
 
 
 # ![](files/attachments/LinearRegression/linregress.png)
-# 
+#
 # Another example: [using scipy (and R) to calculate Linear Regressions](https://warwick.ac.uk/fac/sci/moac/people/students/peter_cock/python/lin_reg)
-
-# In[ ]:
-
-
-
-
