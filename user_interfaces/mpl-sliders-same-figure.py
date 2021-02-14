142d2eee6b33a4354e7f8430c2fa3e02920fb9c1
tnakaicode <tnakaicode@gmail.com>
2021-02-14 13:42:58 +0900

    basemap example

---
+++ user_interfaces/mpl-sliders-same-figure.py
@@ -0,0 +1,27 @@
+"""
+=====================================================
+Matplotlib Sliders without a separate Controls Figure
+=====================================================
+
+Demonstration of how to provide a matplotlib slider to prevent
+the creation of a separate controls figure.
+"""
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib.widgets import Slider
+
+from mpl_interactions import ipyplot as iplt
+
+fig, ax = plt.subplots()
+plt.subplots_adjust(bottom=0.25)
+x = np.linspace(0, 2 * np.pi, 200)
+
+
+def f(x, freq):
+    return np.sin(x * freq)
+
+
+axfreq = plt.axes([0.25, 0.1, 0.65, 0.03])
+slider = Slider(axfreq, label="freq", valmin=0.05, valmax=10)
+controls = iplt.plot(x, f, freq=slider, ax=ax)
+plt.show()
