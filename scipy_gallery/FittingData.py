4e03fd8bf39f3d40dcdf4fc4cfb5083d4ca8b648
tnakaicode <tnakaicode@gmail.com>
2022-03-05 15:17:21 +0900

    fomat

--- scipy_gallery/FittingData.py
+++ scipy_gallery/FittingData.py
@@ -3,21 +3,19 @@
 
 # Fitting data
 # ======================================================================
-# 
+#
 # This page shows you how to fit experimental data and plots the results
 # using matplotlib.
-# 
+#
 # <TableOfContents>
-# 
+#
 # Fit examples with sinusoidal functions
 # --------------------------------------
-# 
+#
 # ### Generating the data
-# 
+#
 # Using real data is much more fun, but, just so that you can reproduce
 # this example I will generate data to fit
-
-# In[1]:
 
 
 import numpy as np
@@ -30,31 +28,36 @@ num_points = 150
 Tx = np.linspace(5., 8., num_points)
 Ty = Tx
 
-tX = 11.86*np.cos(2*pi/0.81*Tx-1.32) + 0.64*Tx+4*((0.5-np.random.rand(num_points))*np.exp(2*np.random.rand(num_points)**2))
-tY = -32.14*np.cos(2*np.pi/0.8*Ty-1.94) + 0.15*Ty+7*((0.5-np.random.rand(num_points))*np.exp(2*np.random.rand(num_points)**2))
+tX = 11.86 * np.cos(2 * pi / 0.81 * Tx - 1.32) + 0.64 * Tx + 4 * (
+    (0.5 - np.random.rand(num_points)) * np.exp(2 * np.random.rand(num_points)**2))
+tY = -32.14 * np.cos(2 * np.pi / 0.8 * Ty - 1.94) + 0.15 * Ty + 7 * (
+    (0.5 - np.random.rand(num_points)) * np.exp(2 * np.random.rand(num_points)**2))
 
 
 # ### Fitting the data
-# 
+#
 # We now have two sets of data: Tx and Ty, the time series, and tX and tY,
 # sinusoidal data with noise. We are interested in finding the frequency
 # of the sine wave.
 
-# In[2]:
-
 
 # Fit the first set
-fitfunc = lambda p, x: p[0]*np.cos(2*np.pi/p[1]*x+p[2]) + p[3]*x # Target function
-errfunc = lambda p, x, y: fitfunc(p, x) - y # Distance to the target function
-p0 = [-15., 0.8, 0., -1.] # Initial guess for the parameters
+def fitfunc(p, x): return p[0] * np.cos(2 * np.pi /
+                                        p[1] * x + p[2]) + p[3] * x  # Target function
+def errfunc(p, x, y): return fitfunc(p, x) - \
+    y  # Distance to the target function
+
+
+p0 = [-15., 0.8, 0., -1.]  # Initial guess for the parameters
 p1, success = optimize.leastsq(errfunc, p0[:], args=(Tx, tX))
 
 time = np.linspace(Tx.min(), Tx.max(), 100)
-plt.plot(Tx, tX, "ro", time, fitfunc(p1, time), "r-") # Plot of the data and the fit
+plt.plot(Tx, tX, "ro", time, fitfunc(p1, time),
+         "r-")  # Plot of the data and the fit
 
 # Fit the second set
 p0 = [-15., 0.8, 0., -1.]
-p2,success = optimize.leastsq(errfunc, p0[:], args=(Ty, tY))
+p2, success = optimize.leastsq(errfunc, p0[:], args=(Ty, tY))
 
 time = np.linspace(Ty.min(), Ty.max(), 100)
 plt.plot(Ty, tY, "b^", time, fitfunc(p2, time), "b-")
@@ -68,17 +71,15 @@ plt.legend(('x position', 'x fit', 'y position', 'y fit'))
 ax = plt.axes()
 
 plt.text(0.8, 0.07,
-         'x freq :  %.3f kHz \n y freq :  %.3f kHz' % (1/p1[1],1/p2[1]),
+         'x freq :  %.3f kHz \n y freq :  %.3f kHz' % (1 / p1[1], 1 / p2[1]),
          fontsize=16,
          horizontalalignment='center',
          verticalalignment='center',
          transform=ax.transAxes)
 
-plt.show()
-
 
 # ### A clever use of the cost function
-# 
+#
 # Suppose that you have the same data set: two time-series of oscillating
 # phenomena, but that you know that the frequency of the two oscillations
 # is the same. A clever use of the cost function can allow you to fit both
@@ -87,11 +88,11 @@ plt.show()
 # data sets for one choice of parameters. Thus the leastsq routine is
 # optimizing both data sets at the same time.
 
-# In[3]:
-
 
 # Target function
-fitfunc = lambda T, p, x: p[0]*np.cos(2*np.pi/T*x+p[1]) + p[2]*x
+def fitfunc(T, p, x): return p[0] * np.cos(2 * np.pi / T * x + p[1]) + p[2] * x
+
+
 # Initial guess for the first set's parameters
 p1 = r_[-15., 0., -1.]
 # Initial guess for the second set's parameters
@@ -101,18 +102,23 @@ T = 0.8
 # Vector of the parameters to fit, it contains all the parameters of the problem, and the period of the oscillation is not there twice !
 p = r_[T, p1, p2]
 # Cost function of the fit, compare it to the previous example.
-errfunc = lambda p, x1, y1, x2, y2: r_[
-                fitfunc(p[0], p[1:4], x1) - y1,
-                fitfunc(p[0], p[4:7], x2) - y2
-            ]
+
+
+def errfunc(p, x1, y1, x2, y2): return r_[
+    fitfunc(p[0], p[1:4], x1) - y1,
+    fitfunc(p[0], p[4:7], x2) - y2
+]
+
+
 # This time we need to pass the two sets of data, there are thus four "args".
-p,success = optimize.leastsq(errfunc, p, args=(Tx, tX, Ty, tY))
-time = np.linspace(Tx.min(), Tx.max(), 100) # Plot of the first data and the fit
-plt.plot(Tx, tX, "ro", time, fitfunc(p[0], p[1:4], time),"r-")
+p, success = optimize.leastsq(errfunc, p, args=(Tx, tX, Ty, tY))
+# Plot of the first data and the fit
+time = np.linspace(Tx.min(), Tx.max(), 100)
+plt.plot(Tx, tX, "ro", time, fitfunc(p[0], p[1:4], time), "r-")
 
 # Plot of the second data and the fit
-time = np.linspace(Ty.min(), Ty.max(),100)
-plt.plot(Ty, tY, "b^", time, fitfunc(p[0], p[4:7], time),"b-")
+time = np.linspace(Ty.min(), Ty.max(), 100)
+plt.plot(Ty, tY, "b^", time, fitfunc(p[0], p[4:7], time), "b-")
 
 # Legend the plot
 plt.title("Oscillations in the compressed trap")
@@ -123,7 +129,7 @@ plt.legend(('x position', 'x fit', 'y position', 'y fit'))
 ax = plt.axes()
 
 plt.text(0.8, 0.07,
-         'x freq :  %.3f kHz' % (1/p[0]),
+         'x freq :  %.3f kHz' % (1 / p[0]),
          fontsize=16,
          horizontalalignment='center',
          verticalalignment='center',
@@ -132,28 +138,28 @@ plt.text(0.8, 0.07,
 
 # Simplifying the syntax
 # ----------------------
-# 
+#
 # Especially when using fits for interactive use, the standard syntax for
 # optimize.leastsq can get really long. Using the following script can
 # simplify your life:
-
-# In[4]:
 
 
 import numpy as np
 from scipy import optimize
 
+
 class Parameter:
     def __init__(self, value):
-            self.value = value
+        self.value = value
 
     def set(self, value):
-            self.value = value
+        self.value = value
 
     def __call__(self):
-            return self.value
+        return self.value
 
-def fit(function, parameters, y, x = None):
+
+def fit(function, parameters, y, x=None):
     def f(params):
         i = 0
         for p in parameters:
@@ -161,63 +167,66 @@ def fit(function, parameters, y, x = None):
             i += 1
         return y - function(x)
 
-    if x is None: x = np.arange(y.shape[0])
+    if x is None:
+        x = np.arange(y.shape[0])
     p = [param() for param in parameters]
     return optimize.leastsq(f, p)
 
 
 # Now fitting becomes really easy, for example fitting to a gaussian:
 
-# In[5]:
-
 
 # giving initial parameters
-mu = Parameter(7)                     
+mu = Parameter(7)
 sigma = Parameter(3)
 height = Parameter(5)
 
 # define your function:
-def f(x): return height() * np.exp(-((x-mu())/sigma())**2) 
+
+
+def f(x): return height() * np.exp(-((x - mu()) / sigma())**2)
+
 
 # fit! (given that data is an array with the data to fit)
-data = 10*np.exp(-np.linspace(0, 10, 100)**2) + np.random.rand(100)
-print fit(f, [mu, sigma, height], data)
+data = 10 * np.exp(-np.linspace(0, 10, 100)**2) + np.random.rand(100)
+print(fit(f, [mu, sigma, height], data))
 
 
 # Fitting gaussian-shaped data
 # ----------------------------
-# 
+#
 # ### Calculating the moments of the distribution
-# 
+#
 # Fitting gaussian-shaped data does not require an optimization routine.
 # Just calculating the moments of the distribution is enough, and this is
 # much faster.
-# 
+#
 # However this works only if the gaussian is not cut out too much, and if
 # it is not too small.
 
-# In[6]:
 
+def gaussian(x): return 3 * np.exp(-(30 - x)**2 / 20.)
 
-gaussian = lambda x: 3*np.exp(-(30-x)**2/20.)
 
 data = gaussian(np.arange(100))
 
 plt.plot(data, '.')
 
 X = np.arange(data.size)
-x = np.sum(X*data)/np.sum(data)
-width = np.sqrt(np.abs(np.sum((X-x)**2*data)/np.sum(data)))
+x = np.sum(X * data) / np.sum(data)
+width = np.sqrt(np.abs(np.sum((X - x)**2 * data) / np.sum(data)))
 
 max = data.max()
 
-fit = lambda t : max*np.exp(-(t-x)**2/(2*width**2))
+
+def fit(t): return max * np.exp(-(t - x)**2 / (2 * width**2))
+
 
 plt.plot(fit(X), '-')
 
 
 # ### Fitting a 2D gaussian
-# 
+#
 # Here is robust code to fit a 2D gaussian. It calculates the moments of
 # the data to guess the initial parameters for an optimization routine.
 # For a more complete gaussian, one with an optional additive constant and
@@ -225,15 +234,14 @@ plt.plot(fit(X), '-')
 # <http://code.google.com/p/agpy/source/browse/trunk/agpy/gaussfitter.py>.
 # It also allows the specification of a known error.
 
-# In[7]:
-
 
 def gaussian(height, center_x, center_y, width_x, width_y):
     """Returns a gaussian function with the given parameters"""
     width_x = float(width_x)
     width_y = float(width_y)
-    return lambda x,y: height*np.exp(
-                -(((center_x-x)/width_x)**2+((center_y-y)/width_y)**2)/2)
+    return lambda x, y: height * np.exp(
+        -(((center_x - x) / width_x)**2 + ((center_y - y) / width_y)**2) / 2)
+
 
 def moments(data):
     """Returns (height, x, y, width_x, width_y)
@@ -241,28 +249,29 @@ def moments(data):
     moments """
     total = data.sum()
     X, Y = np.indices(data.shape)
-    x = (X*data).sum()/total
-    y = (Y*data).sum()/total
+    x = (X * data).sum() / total
+    y = (Y * data).sum() / total
     col = data[:, int(y)]
-    width_x = np.sqrt(np.abs((np.arange(col.size)-x)**2*col).sum()/col.sum())
+    width_x = np.sqrt(np.abs((np.arange(col.size) - x)
+                      ** 2 * col).sum() / col.sum())
     row = data[int(x), :]
-    width_y = np.sqrt(np.abs((np.arange(row.size)-y)**2*row).sum()/row.sum())
+    width_y = np.sqrt(np.abs((np.arange(row.size) - y)
+                      ** 2 * row).sum() / row.sum())
     height = data.max()
     return height, x, y, width_x, width_y
+
 
 def fitgaussian(data):
     """Returns (height, x, y, width_x, width_y)
     the gaussian parameters of a 2D distribution found by a fit"""
     params = moments(data)
-    errorfunction = lambda p: np.ravel(gaussian(*p)(*np.indices(data.shape)) -
-                                 data)
+    def errorfunction(p): return np.ravel(gaussian(*p)(*np.indices(data.shape)) -
+                                          data)
     p, success = optimize.leastsq(errorfunction, params)
     return p
 
 
 # And here is an example using it:
-
-# In[8]:
 
 
 # Create the gaussian data
@@ -282,26 +291,25 @@ plt.text(0.95, 0.05, """
 x : %.1f
 y : %.1f
 width_x : %.1f
-width_y : %.1f""" %(x, y, width_x, width_y),
-        fontsize=16, horizontalalignment='right',
-        verticalalignment='bottom', transform=ax.transAxes)
+width_y : %.1f""" % (x, y, width_x, width_y),
+         fontsize=16, horizontalalignment='right',
+         verticalalignment='bottom', transform=ax.transAxes)
 
 
 # Fitting a power-law to data with errors
 # ---------------------------------------
-# 
+#
 # ### Generating the data
-# 
+#
 # Generate some data with noise to demonstrate the fitting procedure. Data
 # is generated with an amplitude of 10 and a power-law index of -2.0.
 # Notice that all of our data is well-behaved when the log is taken... you
 # may have to be more careful of this for real data.
 
-# In[9]:
-
 
 # Define function for calculating a power law
-powerlaw = lambda x, amp, index: amp * (x**index)
+def powerlaw(x, amp, index): return amp * (x**index)
+
 
 ##########
 # Generate data points with noise
@@ -309,7 +317,7 @@ powerlaw = lambda x, amp, index: amp * (x**index)
 num_points = 20
 
 # Note: all positive, non-zero data
-xdata = np.linspace(1.1, 10.1, num_points) 
+xdata = np.linspace(1.1, 10.1, num_points)
 ydata = powerlaw(xdata, 10.0, -2.0)     # simulated perfect data
 yerr = 0.2 * ydata                      # simulated errors (10%)
 
@@ -317,7 +325,7 @@ ydata += np.random.randn(num_points) * yerr       # simulated noisy data
 
 
 # ### Fitting the data
-# 
+#
 # If your data is well-behaved, you can fit a power-law function by first
 # converting to a linear equation by using the logarithm. Then use the
 # optimize function to fit a straight line. Notice that we are weighting
@@ -328,8 +336,6 @@ ydata += np.random.randn(num_points) * yerr       # simulated noisy data
 # distribution, [this
 # solution](http://code.google.com/p/agpy/wiki/PowerLaw?ts=1251337886&updated=PowerLaw)
 # is more appropriate.
-
-# In[10]:
 
 
 ##########
@@ -349,8 +355,11 @@ logy = np.log10(ydata)
 logyerr = yerr / ydata
 
 # define our (line) fitting function
-fitfunc = lambda p, x: p[0] + p[1] * x   
-errfunc = lambda p, x, y, err: (y - fitfunc(p, x)) / err
+
+
+def fitfunc(p, x): return p[0] + p[1] * x
+def errfunc(p, x, y, err): return (y - fitfunc(p, x)) / err
+
 
 pinit = [1.0, -1.0]
 out = optimize.leastsq(errfunc, pinit,
@@ -358,20 +367,19 @@ out = optimize.leastsq(errfunc, pinit,
 
 pfinal = out[0]
 covar = out[1]
-print pfinal
-print covar
+print(pfinal)
+print(covar)
 
 index = pfinal[1]
 amp = 10.0**pfinal[0]
 
-indexErr = np.sqrt( covar[1][1] ) 
-ampErr = np.sqrt( covar[0][0] ) * amp
+indexErr = np.sqrt(covar[1][1])
+ampErr = np.sqrt(covar[0][0]) * amp
 
 ##########
 # Plotting data
 ##########
 
-plt.clf()
 plt.subplot(2, 1, 1)
 plt.plot(xdata, powerlaw(xdata, amp, index))     # Fit
 plt.errorbar(xdata, ydata, yerr=yerr, fmt='k.')  # Data
@@ -389,3 +397,4 @@ plt.xlabel('X (log scale)')
 plt.ylabel('Y (log scale)')
 plt.xlim(1.0, 11)
 
+plt.show()
--- scipy_gallery/discrete_bvp.py
+++ scipy_gallery/discrete_bvp.py
@@ -2,9 +2,9 @@
 # coding: utf-8
 
 # # Solving a discrete boundary-value problem in scipy
-# 
+#
 # TAGS: Ordinary differential equations
-# 
+#
 # AUTHORS: Nikolay Mayorov
 
 # ## Mathematical formulation
@@ -29,28 +29,19 @@
 
 # From now on we focus on the discrete version and consider a grid with $n = 100$ ticks for each dimension and set $c = 1$ and $f(u) = u^3$.
 
-# In[1]:
 
-
-get_ipython().run_line_magic('matplotlib', 'inline')
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy.optimize import least_squares
 from scipy.sparse import coo_matrix
 
-
-# In[2]:
-
-
 n = 100
 c = 1
 
 
-# In[3]:
-
-
 def f(u):
     return u**3
+
 
 def f_prime(u):
     return 3 * u**2
@@ -60,14 +51,13 @@ def f_prime(u):
 
 # We define a function computing left-hand sides of each equation. Note that we assume values on the boundary to be fixed at zeros and don't change them during optimization.
 
-# In[4]:
 
-
-def fun(u, n, f, f_prime, c, **kwargs):        
+def fun(u, n, f, f_prime, c, **kwargs):
     v = np.zeros((n + 2, n + 2))
     u = u.reshape((n, n))
     v[1:-1, 1:-1] = u
-    y = v[:-2, 1:-1] + v[2:, 1:-1] + v[1:-1, :-2] + v[1:-1, 2:] - 4 * u + c * f(u)
+    y = v[:-2, 1:-1] + v[2:, 1:-1] + \
+        v[1:-1, :-2] + v[1:-1, 2:] - 4 * u + c * f(u)
     return y.ravel()
 
 
@@ -75,58 +65,50 @@ def fun(u, n, f, f_prime, c, **kwargs):
 
 # It is convenient to precompute indices of rows and columns of nonzero elements in Jacobian. We definte the corresponding function:
 
-# In[5]:
 
-
-def compute_jac_indices(n):        
+def compute_jac_indices(n):
     i = np.arange(n)
     jj, ii = np.meshgrid(i, i)
-    
+
     ii = ii.ravel()
     jj = jj.ravel()
-    
+
     ij = np.arange(n**2)
-    
+
     jac_rows = [ij]
     jac_cols = [ij]
-        
+
     mask = ii > 0
     ij_mask = ij[mask]
     jac_rows.append(ij_mask)
     jac_cols.append(ij_mask - n)
-        
+
     mask = ii < n - 1
     ij_mask = ij[mask]
     jac_rows.append(ij_mask)
-    jac_cols.append(ij_mask + n)    
-    
+    jac_cols.append(ij_mask + n)
+
     mask = jj > 0
     ij_mask = ij[mask]
     jac_rows.append(ij_mask)
-    jac_cols.append(ij_mask - 1)    
-    
+    jac_cols.append(ij_mask - 1)
+
     mask = jj < n - 1
     ij_mask = ij[mask]
     jac_rows.append(ij_mask)
     jac_cols.append(ij_mask + 1)
-    
-    return np.hstack(jac_rows), np.hstack(jac_cols)    
+
+    return np.hstack(jac_rows), np.hstack(jac_cols)
 
 
 # After that computing Jacobian in `coo_matrix` format is simple:
 
-# In[6]:
-
-
 jac_rows, jac_cols = compute_jac_indices(n)
 
 
-# In[7]:
-
-
 def jac(u, n, f, f_prime, c, jac_rows=None, jac_cols=None):
-    jac_values = np.ones_like(jac_cols, dtype=float)    
-    jac_values[:n**2] = -4 + c * f_prime(u)    
+    jac_values = np.ones_like(jac_cols, dtype=float)
+    jac_values[:n**2] = -4 + c * f_prime(u)
     return coo_matrix((jac_values, (jac_rows, jac_cols)), shape=(n**2, n**2))
 
 
@@ -134,36 +116,27 @@ def jac(u, n, f, f_prime, c, jac_rows=None, jac_cols=None):
 
 # Without any insight to the problem we set all the values to 0.5 initially. Note, that it is not guranteed that the given continuous or discrete problems have a unique solution.
 
-# In[8]:
-
 
 u0 = np.ones(n**2) * 0.5
 
 
 # Precompute rows and columns of nonzero elements in Jacobian:
 
-# In[9]:
-
-
 jac_rows, jac_cols = compute_jac_indices(n)
 
 
 # Now we are ready to run the optimization. The first solution will be computed without imposing any bounds on the variables.
 
-# In[10]:
-
-
-res_1 = least_squares(fun, u0, jac=jac, gtol=1e-3, args=(n, f, f_prime, c), kwargs={'jac_rows': jac_rows, 'jac_cols': jac_cols}, verbose=1)
+res_1 = least_squares(fun, u0, jac=jac, gtol=1e-3, args=(n, f, f_prime, c),
+                      kwargs={'jac_rows': jac_rows, 'jac_cols': jac_cols}, verbose=1)
 
 
 # Below we visualize the first solution. The left plot shows the flatten solution, the middle plot shows how the solution looks in the square domain, and the right plot shows final residuals in each node.
 
-# In[11]:
-
-
 plt.figure(figsize=(16, 5))
 plt.subplot(132)
-plt.imshow(res_1.x.reshape((n, n)), cmap='coolwarm', vmin=-max(abs(res_1.x)), vmax=max(abs(res_1.x)))
+plt.imshow(res_1.x.reshape((n, n)), cmap='coolwarm',
+           vmin=-max(abs(res_1.x)), vmax=max(abs(res_1.x)))
 plt.colorbar(use_gridspec=True, fraction=0.046, pad=0.04)
 plt.subplot(131)
 plt.plot(res_1.x)
@@ -174,14 +147,9 @@ plt.tight_layout()
 
 # It is possible that some physical considerations require that the solution must be non-negative everywhere. We can achieve that by specifying bounds to the solver:
 
-# In[12]:
-
 
 res_2 = least_squares(fun, u0, jac=jac, bounds=(0, np.inf), gtol=1e-3,
                       args=(n, f, f_prime, c), kwargs={'jac_rows': jac_rows, 'jac_cols': jac_cols}, verbose=1)
-
-
-# In[13]:
 
 
 plt.figure(figsize=(16, 5))
@@ -199,8 +167,4 @@ plt.tight_layout()
 
 # You can try running optimization from different starting points, using different bounds or changing $c$ and $f(u)$, and see how it affects the result from `least_squares` solver.
 
-# In[ ]:
-
-
-
-
+plt.show()
