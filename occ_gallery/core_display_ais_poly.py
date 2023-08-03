5c01e5d6e2425dbd17593cb5ecc973982f491732
tnakaicode <tnakaicode@gmail.com>
2023-08-03 13:23:21 +0900

    occ issue

--- occ_gallery/core_display_ais_poly.py
+++ occ_gallery/core_display_ais_poly.py
@@ -23,18 +23,17 @@ from OCC.Core.gp import gp_XY, gp_Pnt, gp_Dir, gp_Ax1
 from OCC.Core.Geom import Geom_Line
 from OCC.Core.Poly import Poly_ListOfTriangulation, Poly_Triangulation, Poly_Triangle, Poly_Array1OfTriangle
 from OCC.Core.TColgp import TColgp_Array1OfPnt
+from OCC.Core.BRep import BRep_Tool, BRep_Builder
+from OCC.Core.gp import gp_Pnt
+from OCC.Core.TopoDS import TopoDS_Compound
+from OCC.Extend.Construct import make_polygon, make_face
 
 from OCC.Display.SimpleGui import init_display
 
 display, start_display, add_menu, add_function_to_menu = init_display()
 
-myBox = BRepPrimAPI_MakeBox(60, 60, 50).Shape()
-
-line = Geom_Line(gp_Ax1(gp_Pnt(-100, 0, 0), gp_Dir()))
-axis = AIS_Axis(gp_Ax1())
-
-tam = [1 * np.pi, 1 * np.pi, 1 * np.pi]
-spacing = [0.1, 0.1, 0.1]
+tam = [2 * np.pi, 2 * np.pi, 2 * np.pi]
+spacing = [0.25, 0.25, 0.25]
 hole_size = [-0.3, 0.3]
 
 tx, ty, tz = tam
@@ -50,11 +49,10 @@ m = np.array(((f > neg) & (f < pos)) * 1.0)
 
 from skimage import measure
 verts, faces, norm, val = measure.marching_cubes(m, level=None)
-#pts = TColgp_Array1OfPnt(1, verts.shape[0])
-#tri = Poly_Array1OfTriangle(1, faces.shape[0])
 
 print(verts.shape[0], faces.shape[0])
 poly = Poly_Triangulation(verts.shape[0], faces.shape[0], False)
+poly_dat = verts[faces]
 for i, xyz in enumerate(verts):
     poly.SetNode(i + 1, gp_Pnt(float(xyz[0]), float(xyz[1]), float(xyz[2])))
 for i, ixyz in enumerate(faces):
@@ -63,6 +61,36 @@ print(poly.NbNodes(), poly.NbTriangles())
 print(poly.UnloadDeferredData())
 ais_poly = AIS_Triangulation(poly)
 
+bild1 = BRep_Builder()
+comp1 = TopoDS_Compound()
+bild1.MakeCompound(comp1)
+for i, dat in enumerate(poly_dat):
+    p0 = gp_Pnt(float(dat[0, 0]), float(dat[0, 1]), float(dat[0, 2]))
+    p1 = gp_Pnt(float(dat[1, 0]), float(dat[1, 1]), float(dat[1, 2]))
+    p2 = gp_Pnt(float(dat[2, 0]), float(dat[2, 1]), float(dat[2, 2]))
+    poly = make_polygon([p0, p1, p2], closed=True)
+    face = make_face(poly, True)
+    # bild1.Add(comp1, poly)
+    bild1.Add(comp1, face)
+
 display.Context.Display(ais_poly, True)
-# display.DisplayShape(poly)
+display.DisplayShape(comp1)
+display.FitAll()
 start_display()
+
+"""
+Triply Peridic Minimal Surface (TPMS)を生成する方法はないでしょうか？
+
+GyroidはTPMSの一種です。
+私のコードはmarching cubeによって、三角形要素で表現しているため、平面の集まりになっているだけで曲面ではありません。
+また、Node数が増えるとファイルサイズが膨大になります。
+B-Splineのように、曲面として形状を生成する方法を探しています。
+
+How to generate Triply Peridic Minimal Surface (TPMS)?
+
+Gyroid is a type of TPMS. 
+My code is represented by a triangular element by means of a marching cube, which is just a collection of planes and not a curved surface. 
+Also, the file size becomes huge when the number of Nodes increases. 
+I am looking for a way to generate shapes as curved surfaces, like B-Spline. 
+
+"""
