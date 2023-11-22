dc5396be9f322b79a35687116c0fe24f5075f73e
tnakaicode <tnakaicode@gmail.com>
2023-11-22 14:43:27 +0900

    issue

--- occ_gallery/core_topology_fillet_3d.py
+++ occ_gallery/core_topology_fillet_3d.py
@@ -19,13 +19,10 @@ import numpy as np
 from math import cos, pi
 
 from OCC.Core.BRep import BRep_Builder
-from OCC.Core.BRepAlgoAPI import BRepAlgoAPI_Fuse
 from OCC.Core.BRepFilletAPI import BRepFilletAPI_MakeFillet
-from OCC.Core.BRepPrimAPI import BRepPrimAPI_MakeBox, BRepPrimAPI_MakeCylinder
 from OCC.Core.TopoDS import TopoDS_Shell
 from OCC.Core.TopLoc import TopLoc_Location
 from OCC.Display.SimpleGui import init_display
-from OCC.Core.TColgp import TColgp_Array1OfPnt2d
 from OCC.Core.gp import gp_Ax2, gp_Pnt, gp_Dir, gp_Pnt2d, gp_Ax1, gp_Trsf
 from OCC.Core.LocOpe import LocOpe_FindEdges
 from OCC.Extend.TopologyUtils import TopologyExplorer
@@ -61,6 +58,7 @@ display.DisplayShape(box1_shell, transparency=0.7)
 box2 = make_box(gp_Pnt(40, 0, 0), 20, 20, 20)
 box2_faces = list(TopologyExplorer(box2).faces())
 
+# Rotate 0 degree
 box2_trsf = gp_Trsf()
 box2_trsf.SetRotation(gp_Ax1(gp_Pnt(40, 0, 0), gp_Dir(0, 0, 1)), np.deg2rad(0))
 box2_faces[0].Move(TopLoc_Location(box2_trsf))
@@ -80,7 +78,8 @@ fillet_edge = find_edge.EdgeTo()
 # Create Fillet of R5 on shared Edge.
 fillet = BRepFilletAPI_MakeFillet(box2_shell, 0)
 fillet.Add(5, fillet_edge)
-# fillet.Build() # Get Error
+# fillet.Build() 
+# RuntimeError: Standard_Failure There are no suitable edges for chamfer or fillet raised from method Build of class BRepBuilderAPI_MakeShape
 if fillet.IsDone():
     display.DisplayShape(fillet.Shape())
 else:
@@ -90,3 +89,13 @@ display.DisplayShape(box2_shell, transparency=0.7)
 
 display.FitAll()
 start_display()
+
+# MakeFillet between 2 faces is Failure
+# 
+# 共有辺を持つ2面の間にFilletを作ろうとしたところ、fillet.Build()の段階で失敗した。
+# 以下のコードでは、MakeBoxで作ったboxから辺を共有する2面を取り出してFilletを作っている。
+# box1では成功するが、面の位置を変えす(0度回転)明らかに共有する辺があるのにbox2では失敗する。
+# When I tried to create a Fillet between two faces with shared edges, it failed at the fillet.Build() stage.
+# In my code below, Fillet is created by taking 2 faces that share an edge from a box created by MakeBox.
+# It succeeds in box1, but fails in box2 when the faces are repositioned (rotated 0 degrees) and there are clearly shared edges.
+# 
