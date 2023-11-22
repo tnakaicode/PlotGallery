558ee45118fb72fb757b0d24a160f9bf671015af
tnakaicode <tnakaicode@gmail.com>
2023-11-22 15:09:46 +0900

    split box

--- occ_gallery/core_topology_fillet_box.py
+++ occ_gallery/core_topology_fillet_box.py
@@ -34,10 +34,11 @@ from OCCUtils.Construct import make_box, make_plane
 display, start_display, add_menu, add_function_to_menu = init_display()
 
 
-def split_box(box, pln):
+def split_box(box, plns):
     splitter = BOPAlgo_Splitter()
     splitter.AddArgument(box)
-    splitter.AddTool(pln)
+    for p in plns:
+        splitter.AddTool(p)
     splitter.Perform()
 
     shps = []
@@ -48,10 +49,16 @@ def split_box(box, pln):
     return shps
 
 
-pln = make_plane(gp_Pnt(0, 0, 10), gp_Vec(
-    0, 0.5, 0.8), -1000, 1000, -1000, 1000)
+pln1 = make_plane(gp_Pnt(0, 0, 50),
+                  gp_Vec(0, 0.5, 0.8),
+                  -1000, 1000, -1000, 1000)
 
-box1 = make_box(gp_Pnt(0, 0, 0), 20, 20, 20)
+pln2 = make_plane(gp_Pnt(50, 0, 0),
+                  gp_Vec(0.5, 0.0, 0.5),
+                  -1000, 1000, -1000, 1000)
+pln = [pln1, pln2]
+
+box1 = make_box(gp_Pnt(0, 0, 0), 100, 100, 100)
 box1 = split_box(box1, pln)[0]
 box1_faces = list(TopologyExplorer(box1).faces())
 
@@ -59,17 +66,17 @@ box1_faces = list(TopologyExplorer(box1).faces())
 box1_shell = TopoDS_Shell()
 bild = BRep_Builder()
 bild.MakeShell(box1_shell)
-for shp in [box1_faces[3], box1_faces[2]]:
+for shp in [box1_faces[4], box1_faces[2], box1_faces[3]]:
     bild.Add(box1_shell, shp)
 
 # Find Edge that the two faces share
-find_edge = LocOpe_FindEdges(box1_faces[3], box1_faces[2])
+find_edge = LocOpe_FindEdges(box1_faces[4], box1_faces[2])
 find_edge.InitIterator()
 fillet_edge = find_edge.EdgeTo()
 
 # Create Fillet of R5 on shared Edge.
 fillet = BRepFilletAPI_MakeFillet(box1_shell, 0)
-fillet.Add(2.5, fillet_edge)
+fillet.Add(10.0, fillet_edge)
 fillet.Build()
 if fillet.IsDone():
     display.DisplayShape(fillet.Shape())
@@ -79,7 +86,7 @@ display.DisplayShape(box1, transparency=0.7)
 display.DisplayShape(fillet_edge, color="BLUE1")
 display.DisplayShape(box1_shell, transparency=0.7, color="RED")
 
-box2 = make_box(gp_Pnt(40, 0, 0), 20, 20, 20)
+box2 = make_box(gp_Pnt(110, 0, 0), 20, 20, 20)
 box2 = split_box(box2, pln)[0]
 box2_faces = list(TopologyExplorer(box2).faces())
 
