f2bb330229b356d238dfdd0c4384153f66e0fd95
tnakaicode <tnakaicode@gmail.com>
2023-11-27 13:18:55 +0900

    fix

--- occ_gallery/core_shape_properties.py
+++ occ_gallery/core_shape_properties.py
@@ -17,9 +17,10 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with pythonOCC.  If not, see <http://www.gnu.org/licenses/>.
 
+from OCC.Core.gp import gp_Ax2, gp_Pnt, gp_Dir
 from OCC.Core.BRepPrimAPI import BRepPrimAPI_MakeBox
 from OCC.Core.GProp import GProp_GProps
-from OCC.Core.BRepGProp import brepgprop_VolumeProperties, brepgprop_SurfaceProperties
+from OCC.Core.BRepGProp import brepgprop
 
 from OCC.Extend.TopologyUtils import TopologyExplorer
 
@@ -28,10 +29,10 @@ def cube_inertia_properties():
     """Compute the inertia properties of a shape"""
     # Create and display cube
     print("Creating a cubic box shape (50*50*50)")
-    cube_shape = BRepPrimAPI_MakeBox(50.0, 50.0, 50.0).Shape()
+    cube_shape = BRepPrimAPI_MakeBox(gp_Ax2(gp_Pnt(0,0,0), gp_Dir(0,0.5,1)), 50.0, 50.0, 50.0).Shape()
     # Compute inertia properties
     props = GProp_GProps()
-    brepgprop_VolumeProperties(cube_shape, props)
+    brepgprop.VolumeProperties(cube_shape, props)
     # Get inertia properties
     mass = props.Mass()
     cog = props.CentreOfMass()
@@ -40,7 +41,9 @@ def cube_inertia_properties():
     print("Cube mass = %s" % mass)
     cog_x, cog_y, cog_z = cog.Coord()
     print("Center of mass: x = %f;y = %f;z = %f;" % (cog_x, cog_y, cog_z))
-    print("Matrix of inertia", matrix_of_inertia)
+    print("Matrix of inertia", matrix_of_inertia.Value(1,1), matrix_of_inertia.Value(1,2), matrix_of_inertia.Value(1,3))
+    print("                 ", matrix_of_inertia.Value(2,1), matrix_of_inertia.Value(2,2), matrix_of_inertia.Value(2,3))
+    print("                 ", matrix_of_inertia.Value(3,1), matrix_of_inertia.Value(3,2), matrix_of_inertia.Value(3,3))
 
 
 def shape_faces_surface():
@@ -52,7 +55,7 @@ def shape_faces_surface():
     props = GProp_GProps()
     shp_idx = 1
     for face in t.faces():
-        brepgprop_SurfaceProperties(face, props)
+        brepgprop.SurfaceProperties(face, props)
         face_surf = props.Mass()
         print("Surface for face nbr %i : %f" % (shp_idx, face_surf))
         shp_idx += 1
