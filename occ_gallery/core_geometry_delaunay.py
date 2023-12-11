d5ca3287add689a29cd2dec5780cc7625d2363b1
tnakaicode <tnakaicode@gmail.com>
2023-12-11 13:07:47 +0900

    format

--- occ_gallery/core_geometry_delaunay.py
+++ occ_gallery/core_geometry_delaunay.py
@@ -143,60 +143,6 @@ def valid(e, basel):
     return ccw(dest(e), dest(basel), org(basel))
 
 
-class Mesh:
-    """
-    Used to hold state of the mesh and
-    to collect imperative functions regarding
-    i/o, generation and deletion.
-    """
-
-    def __init__(self):
-        self.quadEdges = []
-        self.vertices = []
-        self.nextEdge = 0
-        self.rows = 1
-        self.qid = 0
-
-    def makeEdge(self, org, dest):
-        """Construct Edges, adds QuadEdge to mesh. See Q&S"""
-
-        qe = QuadEdge(org, dest, id=str(self.qid))
-        qe[1].next = qe[3]
-        qe[3].next = qe[1]
-
-        self.quadEdges.append(qe)
-        self.qid += 1
-
-        return qe[0]
-
-    def connect(self, a, b):
-        """Connect two edges by a third, while making shure
-        they share the same left face. See Q&S"""
-
-        c = self.makeEdge(dest(a), org(b))
-        splice(c, lnext(a))
-        splice(sym(c), b)
-
-        return c
-
-    def deleteEdge(self, e):
-        """Delete edge without changing the topology"""
-
-        splice(e, oprev(e))
-        splice(sym(e), oprev(sym(e)))
-        # e.parent.edges.remove(e)
-        self.quadEdges.remove(e.parent)
-
-        return
-
-    def loadVertices(self, vertList):
-        """Sort vertices on the x-axis"""
-
-        self.vertices = sorted(vertList, key=lambda vert: vert.x)
-
-        return
-
-
 class QuadEdge:
 
     """Container for Edges, which can be accessed by get/setitem"""
@@ -264,154 +210,208 @@ class Vertex:
         return self.__hash__()
 
 
-def delaunay(m, start, end, leftEdge=None, rightEdge=None, rows=None):
+class Mesh:
     """
-    This is an implentation of the divide-and-conquer alorithm proposed
-    by guibas & stolfi.
-
-    It returns the outermost edges [leftEdge, rightEdge] of the convex
-    hull it builds while connecting points to form a delaunay triangulation.
+    Used to hold state of the mesh and
+    to collect imperative functions regarding
+    i/o, generation and deletion.
     """
 
-    verts = m.vertices
-    if start < (end - 2):  # four or more points
-        # divide points in two halves
-        split = (end - start) // 2 + start
+    def __init__(self):
+        self.quadEdges = []
+        self.vertices = []
+        self.nextEdge = 0
+        self.rows = 1
+        self.qid = 0
 
-        # recurse down the halves
-        ldo, ldi = delaunay(m, start, split)
-        rdi, rdo = delaunay(m, (split + 1), end)
+    def makeEdge(self, org, dest):
+        """Construct Edges, adds QuadEdge to mesh. See Q&S"""
 
-        # 'Compute the lower common tangent of L and R'
-        while True:
-            if ccw(org(rdi), org(ldi), dest(ldi)):  # leftOf
-                ldi = lnext(ldi)
-            elif ccw(org(ldi), dest(rdi), org(rdi)):  # rightOf
-                rdi = rprev(rdi)
-            else:
-                break
+        qe = QuadEdge(org, dest, id=str(self.qid))
+        qe[1].next = qe[3]
+        qe[3].next = qe[1]
 
-        # 'Create a first cross edge basel from rdi.Org to ldi.Org'
-        basel = m.connect(sym(rdi), ldi)
-        if org(ldi) == org(ldo):
-            ldo = sym(basel)
-        if org(rdi) == org(rdo):
-            rdo = basel
+        self.quadEdges.append(qe)
+        self.qid += 1
 
-        # merge the obtained halves
-        # merge(m, ldo, ldi, rdi, rdo)
+        return qe[0]
 
-        while True:
-            """
-            'Locate the first L point (lcand.Dest) to be encountered
-             by the rising bubble and delete L edges out of basel.Dest'
-            """
-            lcand = rprev(basel)
-            if valid(lcand, basel):
-                while inCircle(
-                    dest(basel), org(basel), dest(lcand), dest(onext(lcand))
+    def connect(self, a, b):
+        """Connect two edges by a third, while making shure
+        they share the same left face. See Q&S"""
+
+        c = self.makeEdge(dest(a), org(b))
+        splice(c, lnext(a))
+        splice(sym(c), b)
+
+        return c
+
+    def deleteEdge(self, e):
+        """Delete edge without changing the topology"""
+
+        splice(e, oprev(e))
+        splice(sym(e), oprev(sym(e)))
+        # e.parent.edges.remove(e)
+        self.quadEdges.remove(e.parent)
+
+        return
+
+    def loadVertices(self, vertList):
+        """Sort vertices on the x-axis"""
+
+        self.vertices = sorted(vertList, key=lambda vert: vert.x)
+
+        return
+
+    def delaunay(self, start=0, end=100, leftEdge=None, rightEdge=None, rows=None):
+        """
+        This is an implentation of the divide-and-conquer alorithm proposed
+        by guibas & stolfi.
+
+        It returns the outermost edges [leftEdge, rightEdge] of the convex
+        hull it builds while connecting points to form a delaunay triangulation.
+        """
+
+        verts = self.vertices
+        if start < (end - 2):  # four or more points
+            # divide points in two halves
+            split = (end - start) // 2 + start
+
+            # recurse down the halves
+            ldo, ldi = self.delaunay(start, split)
+            rdi, rdo = self.delaunay((split + 1), end)
+
+            # 'Compute the lower common tangent of L and R'
+            while True:
+                if ccw(org(rdi), org(ldi), dest(ldi)):  # leftOf
+                    ldi = lnext(ldi)
+                elif ccw(org(ldi), dest(rdi), org(rdi)):  # rightOf
+                    rdi = rprev(rdi)
+                else:
+                    break
+
+            # 'Create a first cross edge basel from rdi.Org to ldi.Org'
+            basel = self.connect(sym(rdi), ldi)
+            if org(ldi) == org(ldo):
+                ldo = sym(basel)
+            if org(rdi) == org(rdo):
+                rdo = basel
+
+            # merge the obtained halves
+            # merge(m, ldo, ldi, rdi, rdo)
+
+            while True:
+                """
+                'Locate the first L point (lcand.Dest) to be encountered
+                 by the rising bubble and delete L edges out of basel.Dest'
+                """
+                lcand = rprev(basel)
+                if valid(lcand, basel):
+                    while inCircle(
+                        dest(basel), org(basel), dest(
+                            lcand), dest(onext(lcand))
+                    ):
+                        t = onext(lcand)
+                        self.deleteEdge(lcand)
+                        lcand = t
+
+                """
+                'Symmetrically, locate the first R point to be hit
+                 and delete R edges'
+                """
+                rcand = oprev(basel)
+                if valid(rcand, basel):
+                    while inCircle(
+                        dest(basel), org(basel), dest(
+                            rcand), dest(oprev(rcand))
+                    ):
+                        t = oprev(rcand)
+                        self.deleteEdge(rcand)
+                        rcand = t
+
+                """
+                'If both lcand and rcand are invalid,
+                 then basel is the upper common tangent'
+                """
+
+                lvalid = valid(lcand, basel)
+                rvalid = valid(rcand, basel)
+
+                if not lvalid and not rvalid:
+                    break
+
+                """
+                'The next cross edge is to be connected to either
+                 lcand.Dest or rcand.Dest. If both are valid, then
+                 choose the appropriate one using the InCircle test:'
+                """
+                if (
+                    not lvalid
+                    or rvalid
+                    and inCircle(dest(lcand), org(lcand), org(rcand), dest(rcand))
                 ):
-                    t = onext(lcand)
-                    m.deleteEdge(lcand)
-                    lcand = t
+                    # 'Add cross edge basel from rcand.Dest to basel.Dest'
+                    basel = self.connect(rcand, sym(basel))
 
+                else:
+                    # 'Add cross edge basel from basel.Org to lcand.Dest'
+                    basel = self.connect(sym(basel), sym(lcand))
+
+            xMin = verts[start]
+            xMax = verts[end]
+            while org(ldo) != xMin:
+                ldo = rprev(ldo)
+
+            while org(rdo) != xMax:
+                rdo = lprev(rdo)
+
+            return [ldo, rdo]
+
+        elif start >= (end - 1):  # two or one points
             """
-            'Symmetrically, locate the first R point to be hit
-             and delete R edges'
-            """
-            rcand = oprev(basel)
-            if valid(rcand, basel):
-                while inCircle(
-                    dest(basel), org(basel), dest(rcand), dest(oprev(rcand))
-                ):
-                    t = oprev(rcand)
-                    m.deleteEdge(rcand)
-                    rcand = t
-
-            """
-            'If both lcand and rcand are invalid,
-             then basel is the upper common tangent'
+            Original Comment:
+             'Let s1, s2 be the two sites, in sorted order.
+              Create an edge a from s1 to s2'
             """
 
-            lvalid = valid(lcand, basel)
-            rvalid = valid(rcand, basel)
+            a = self.makeEdge(verts[start], verts[end])
 
-            if not lvalid and not rvalid:
-                break
+            if start == end:
+                exit()
 
+            return [a, sym(a)]
+
+        else:  # Three points
             """
-            'The next cross edge is to be connected to either
-             lcand.Dest or rcand.Dest. If both are valid, then
-             choose the appropriate one using the InCircle test:'
+            Original Comment:
+             'Let s1, s2, s3 be the three sites, in sorted order.
+              Create edges a connecting s1 to s2 and b connecting s2 to s3'
             """
-            if (
-                not lvalid
-                or rvalid
-                and inCircle(dest(lcand), org(lcand), org(rcand), dest(rcand))
-            ):
-                # 'Add cross edge basel from rcand.Dest to basel.Dest'
-                basel = m.connect(rcand, sym(basel))
 
-            else:
-                # 'Add cross edge basel from basel.Org to lcand.Dest'
-                basel = m.connect(sym(basel), sym(lcand))
+            assert len(verts[start: end + 1]) == 3  # just in case
 
-        xMin = verts[start]
-        xMax = verts[end]
-        while org(ldo) != xMin:
-            ldo = rprev(ldo)
+            v1, v2, v3 = verts[start: end + 1]
+            a = self.makeEdge(v1, v2)
+            b = self.makeEdge(v2, v3)
+            splice(sym(a), b)
 
-        while org(rdo) != xMax:
-            rdo = lprev(rdo)
+            """'Now close the triangle'"""
 
-        return [ldo, rdo]
+            if ccw(v1, v3, v2):
+                c = self.connect(b, a)
+                return [sym(c), c]
 
-    elif start >= (end - 1):  # two or one points
-        """
-        Original Comment:
-         'Let s1, s2 be the two sites, in sorted order.
-          Create an edge a from s1 to s2'
-        """
+            elif ccw(v1, v2, v3):
+                c = self.connect(b, a)
+                return [a, sym(b)]
 
-        a = m.makeEdge(verts[start], verts[end])
+            else:  # points are colinear
+                self.deleteEdge(c)
+                return [a, sym(b)]
 
-        if start == end:
-            exit()
-
-        return [a, sym(a)]
-
-    else:  # Three points
-        """
-        Original Comment:
-         'Let s1, s2, s3 be the three sites, in sorted order.
-          Create edges a connecting s1 to s2 and b connecting s2 to s3'
-        """
-
-        assert len(verts[start: end + 1]) == 3  # just in case
-
-        v1, v2, v3 = verts[start: end + 1]
-        a = m.makeEdge(v1, v2)
-        b = m.makeEdge(v2, v3)
-        splice(sym(a), b)
-
-        """'Now close the triangle'"""
-
-        if ccw(v1, v3, v2):
-            c = m.connect(b, a)
-            return [sym(c), c]
-
-        elif ccw(v1, v2, v3):
-            c = m.connect(b, a)
-            return [a, sym(b)]
-
-        else:  # points are colinear
-            m.deleteEdge(c)
-            return [a, sym(b)]
-
-
-from random import seed, uniform
 
 if __name__ == "__main__":
+    from random import seed, uniform
     seed(123123123)
 
     N = 44  # number of vertices
@@ -424,7 +424,7 @@ if __name__ == "__main__":
 
     end = N - 1
 
-    delaunay(m, 0, end)  # computes the triangulation
+    m.delaunay(0, end)  # computes the triangulation
 
     # populates a list of [org, dest], values for further manipulation
     lines = []
