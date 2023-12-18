9486628ab8f3c81baae6a0552039f7b8e862742a
tnakaicode <tnakaicode@gmail.com>
2023-12-18 17:52:06 +0900

    plot graph

--- scikit_rf/plot_circuit.py
+++ scikit_rf/plot_circuit.py
@@ -1,4 +1,5 @@
 import skrf as rf
+import matplotlib.pyplot as plt
 
 # connexions = [
 #     [(port1, 0), (network1, 0), (network4, 0)],
@@ -34,11 +35,68 @@ cnx = [
 # building the circuit
 cir = rf.Circuit(cnx)
 
+cir.plot_graph(network_labels=True, network_fontsize=15,
+               port_labels=True, port_fontsize=15,
+               edge_labels=True, edge_fontsize=10)
+
+
 # getting the resulting Network from the 'network' parameter:
 ntw = cir.network
 print(ntw)
 
-cir.plot_graph()
 
-import matplotlib.pyplot as plt
+# piece of transmission line and series impedance
+trans_line = tline_media.line(theta, unit='deg', name='trans_line')
+load = tline_media.resistor(Z_L, name='delay_load')
+# ground network (short)
+ground = rf.Circuit.Ground(freq, name='ground')
+
+# connexion list
+cnx = [
+    [(port1, 0), (trans_line, 0)],
+    [(trans_line, 1), (load, 0)],
+    [(load, 1), (ground, 0)]
+]
+# building the circuit
+cir = rf.Circuit(cnx)
+
+cir.plot_graph(network_labels=True, network_fontsize=15,
+               port_labels=True, port_fontsize=15,
+               edge_labels=True, edge_fontsize=10)
+
+
+# the result if the same :
+ntw = cir.network
+print(ntw)
+print(cir.network.s[0])
+
+
+freq = rf.Frequency(start=0.1, stop=10, unit='GHz', npoints=1001)
+tl_media = rf.DefinedGammaZ0(freq, z0=50, gamma=1j * freq.w / rf.c)
+C1 = tl_media.capacitor(3.222e-12, name='C1')
+C2 = tl_media.capacitor(82.25e-15, name='C2')
+C3 = tl_media.capacitor(3.222e-12, name='C3')
+L2 = tl_media.inductor(8.893e-9, name='L2')
+RL = tl_media.resistor(50, name='RL')
+gnd = rf.Circuit.Ground(freq, name='gnd')
+port1 = rf.Circuit.Port(freq, name='port1', z0=50)
+port2 = rf.Circuit.Port(freq, name='port2', z0=50)
+
+cnx = [
+    [(port1, 0), (C1, 0), (L2, 0), (C2, 0)],
+    [(L2, 1), (C2, 1), (C3, 0), (port2, 0)],
+    [(gnd, 0), (C1, 1), (C3, 1)],
+]
+cir = rf.Circuit(cnx)
+ntw = cir.network
+
+cir.plot_graph(network_labels=True, network_fontsize=15,
+               port_labels=True, port_fontsize=15,
+               edge_labels=True, edge_fontsize=10)
+plt.figure()
+ntw.plot_s_db(m=0, n=0, lw=2, logx=True)
+ntw.plot_s_db(m=1, n=0, lw=2, logx=True)
+
+print(ntw)
+print(cir.network.s[0])
 plt.show()
