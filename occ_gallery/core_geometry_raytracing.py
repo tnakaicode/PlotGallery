eacef81975d89974bc4d5649647007cd6de78357
tnakaicode <tnakaicode@gmail.com>
2023-12-27 13:31:17 +0900

    ray trace

--- occ_gallery/core_geometry_raytracing.py
+++ occ_gallery/core_geometry_raytracing.py
@@ -15,6 +15,7 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with pythonOCC.  If not, see <http://www.gnu.org/licenses/>.
 
+import numpy as np
 import sys
 
 from OCC.Display.SimpleGui import init_display
@@ -28,46 +29,40 @@ from OCC.Core.Quantity import (
     Quantity_NOC_BROWN,
 )
 from OCC.Core.BRepAlgoAPI import BRepAlgoAPI_Cut
-from OCC.Core.gp import gp_Vec, gp_Pnt
+from OCC.Core.gp import gp_Vec, gp_Pnt, gp_Dir, gp_Ax3, gp_Lin
 
 from OCC.Extend.ShapeFactory import translate_shp
+from OCC.Extend.DataExchange import read_step_file
 
-
-# first create geometry
-from core_classic_occ_bottle import bottle
-
-table = translate_shp(BRepPrimAPI_MakeBox(
-    100, 100, 10).Shape(), gp_Vec(-50, -50, -10))
-glass_out = BRepPrimAPI_MakeCone(7, 9, 25).Shape()
-glass_in = translate_shp(BRepPrimAPI_MakeCone(
-    7, 9, 25).Shape(), gp_Vec(0.0, 0.0, 0.2))
-glass = BRepAlgoAPI_Cut(glass_out, glass_in).Shape()
-translated_glass = translate_shp(glass, gp_Vec(-30, -30, 0))
+from OCC.Core.BRepTools import breptools
+from OCC.Core.TopoDS import TopoDS_Shape
+from OCC.Core.BRep import BRep_Builder
 
 # then inits display
 display, start_display, add_menu, add_function_to_menu = init_display()
 
-# create one spotlight
-spot_light = V3d_SpotLight(
-    gp_Pnt(-100, -100, 100),
-    V3d_XnegYnegZpos,
-    Quantity_Color(Quantity_NOC_WHITE)
-)
-# display the spotlight in rasterized mode
-display.Viewer.AddLight(spot_light)
-display.View.SetLightOn()
+cylinder_head = TopoDS_Shape()
+builder = BRep_Builder()
+breptools.Read(cylinder_head, "../assets/models/cylinder_head.brep", builder)
 
-display.DisplayShape(bottle, material=Graphic3d_NOM_ALUMINIUM)
-display.DisplayShape(table, material=Graphic3d_NOM_PLASTIC,
-                     color=Quantity_NOC_CORAL2)
-display.DisplayShape(
-    translated_glass,
-    material=Graphic3d_NOM_PLASTIC,
-    color=Quantity_NOC_BROWN,
-    transparency=0.6,
-    update=True,
-)
+beam = gp_Ax3(gp_Pnt(10, 0, 0), gp_Dir(1, 1, 1))
 
+from OCC.Core.BRepIntCurveSurface import BRepIntCurveSurface_Inter
+lin = gp_Lin(beam.Location(), beam.Direction())
+api = BRepIntCurveSurface_Inter()
+api.Init(cylinder_head, lin, 1.0E-9)
+dat = []
+while api.More():
+    p = api.Point()
+    p.Dump()
+    data = [p.W(), p.U(), p.V(), p.Pnt(), api.Face(), p.Transition()]
+    dat.append(data)
+    api.Next()
+
+dat.sort(key=lambda e: e[0])
+[display.DisplayShape(d[3]) for d in dat]
+display.DisplayShape(beam.Location())
+display.DisplayShape(cylinder_head)
 
 display.FitAll()
 start_display()
