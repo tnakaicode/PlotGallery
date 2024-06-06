bb4ff8d8d49736d5f6e90eac52aaa8d37020ffe7
tnakaicode <tnakaicode@gmail.com>
2024-06-06 17:32:33 +0900

    format

--- occ_gallery/core_geometry_project_step.py
+++ occ_gallery/core_geometry_project_step.py
@@ -1,21 +1,22 @@
-# https://github.com/tpaviot/pythonocc-core/issues/1326
-from __future__ import print_function
-
-import os
-import os.path
-import sys
 import numpy as np
 
 from OCC.Core.gp import gp_Pnt, gp_Vec, gp_Dir
 from OCC.Core.gp import gp_Ax1, gp_Ax2, gp_Ax3, gp_Trsf
 from OCC.Core.gp import gp_Lin
-from OCC.Core.TopoDS import TopoDS_Edge
-from OCC.Core.BRep import BRep_Tool
-from OCC.Core.BRepAdaptor import BRepAdaptor_Surface
-from OCC.Core.TopLoc import TopLoc_Location
-from OCC.Extend.TopologyUtils import TopologyExplorer
-from OCC.Extend.DataExchange import read_step_file, write_step_file
 
+from OCC.Core.TopoDS import TopoDS_Face
+from OCC.Core.TopAbs import TopAbs_SOLID
+from OCC.Core.TopLoc import TopLoc_Location
+from OCC.Core.TopExp import TopExp_Explorer
+
+from OCC.Core.BRep import BRep_Tool
+from OCC.Core.BRepProj import BRepProj_Projection
+from OCC.Core.BRepAlgo import BRepAlgo_FaceRestrictor
+from OCC.Core.BRepOffsetAPI import BRepOffsetAPI_ThruSections
+from OCC.Core.BRepIntCurveSurface import BRepIntCurveSurface_Inter
+
+from OCC.Core.BOPAlgo import BOPAlgo_Splitter
+from OCC.Extend.DataExchange import read_step_file, write_step_file
 from OCCUtils.Construct import make_polygon, make_wire, make_vertex, make_edge, dir_to_vec
 from OCCUtils.Common import minimum_distance
 
@@ -46,9 +47,6 @@ print(axis.Location())
 lin = gp_Lin(axis.Location(), gp_Dir(0, 1, 0))
 
 
-from OCC.Core.BRepIntCurveSurface import BRepIntCurveSurface_Inter
-from OCC.Core.BRepProj import BRepProj_Projection
-from OCC.Core.BRepAlgo import BRepAlgo_FaceRestrictor
 api_int = BRepIntCurveSurface_Inter()
 api_int.Init(shp, lin, 1.0E-9)
 shp_int = []
@@ -81,9 +79,9 @@ while api_int.More():
                     api_int.V(),  # 2
                     api_int.Pnt(),  # 3
                     api_int.Face(),  # 4
-                    poly,  # 5
-                    face,  # 6
-                    api_int.Transition(),  # 7
+                    api_int.Transition(),  # 5
+                    poly,  # 6
+                    face,  # 7
                     ])
 
     display.DisplayShape(poly, color="BLUE1")
@@ -93,23 +91,19 @@ while api_int.More():
 shp_int.sort(key=lambda e: e[0])
 
 
-from OCC.Core.BRepOffsetAPI import BRepOffsetAPI_ThruSections
 api_thru = BRepOffsetAPI_ThruSections()
 api_thru.SetSmoothing(False)
-api_thru.AddWire(shp_int[0][5])
-api_thru.AddWire(shp_int[1][5])
+api_thru.AddWire(shp_int[0][6])
+api_thru.AddWire(shp_int[1][6])
 api_thru.Build()
 # display.DisplayShape(api.Shape())
 
 
-from OCC.Core.BOPAlgo import BOPAlgo_Splitter
-from OCC.Core.TopExp import TopExp_Explorer
-from OCC.Core.TopAbs import TopAbs_SOLID
 splitter = BOPAlgo_Splitter()
 splitter.AddArgument(shp)
 splitter.AddTool(api_thru.Shape())
-splitter.AddTool(shp_int[0][5])
-splitter.AddTool(shp_int[1][5])
+splitter.AddTool(shp_int[0][7])
+splitter.AddTool(shp_int[1][7])
 splitter.Perform()
 print(splitter.Arguments())
 print(splitter.ShapesSD())
