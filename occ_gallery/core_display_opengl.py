57c68eb3170d10f80cca8e6abcd1bbba94ecbec5
tnakaicode <tnakaicode@gmail.com>
2024-07-23 08:59:20 +0900

    occ display

---
+++ occ_gallery/core_display_opengl.py
@@ -0,0 +1,42 @@
+from __future__ import print_function
+import sys
+from OCC.Display.backend import load_any_qt_backend, get_qt_modules
+load_any_qt_backend()
+from OCC.Display.backend import load_backend
+load_backend("pyside5")
+from PyQt5 import QtWidgets, QtGui, QtCore, QtWidgets, QtOpenGL
+# from PySide6 import QtWidgets, QtGui, QtCore, QtWidgets, QtOpenGL
+# QtCore, QtGui, QtWidgets, QtOpenGL = get_qt_modules()
+from OCC.Display.qtDisplay import qtViewer3d
+# import numpy  as np
+
+
+class GLWidget(qtViewer3d):
+    def __init__(self, parent=None):
+        super(GLWidget, self).__init__(parent)
+
+
+if __name__ == '__main__':
+    def TestOverPainting():
+        class AppFrame(QtWidgets.QWidget):
+            def __init__(self, parent=None):
+                QtWidgets.QWidget.__init__(self, parent)
+                self.setWindowTitle(
+                    self.tr("qtDisplay3d overpainting example"))
+                self.resize(1280, 1024)
+                self.canva = GLWidget(self)
+                mainLayout = QtWidgets.QHBoxLayout()
+                mainLayout.addWidget(self.canva)
+                mainLayout.setContentsMargins(0, 0, 0, 0)
+                self.setLayout(mainLayout)
+
+            def runTests(self):
+                self.canva._display.Test()
+
+        app = QtWidgets.QApplication(sys.argv)
+        frame = AppFrame()
+        frame.show()
+        frame.runTests()
+        app.exec()
+
+    TestOverPainting()
