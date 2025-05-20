377aaec072b9fa4a6556d99054d7eb0b1b132a6e
tnakaicode <tnakaicode@gmail.com>
2025-05-20 17:35:50 +0900

    spline

--- occ_gallery/core_matplotlib_spline_surf.py
+++ occ_gallery/core_matplotlib_spline_surf.py
@@ -1,27 +1,53 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from OCC.Core.TColgp import TColgp_Array2OfPnt
+from scipy.interpolate import bisplrep, bisplev
+from OCC.Core.TColgp import TColgp_Array2OfPnt, TColgp_HArray2OfPnt
 from OCC.Core.GeomAPI import GeomAPI_PointsToBSplineSurface
+from OCC.Core.GeomAbs import GeomAbs_C0, GeomAbs_C1, GeomAbs_C2
+from OCC.Core.GeomAbs import GeomAbs_G1, GeomAbs_G2
+from OCC.Core.BRepAlgoAPI import BRepAlgoAPI_Algo
 from OCC.Core.gp import gp_Pnt
+from OCC.Display.SimpleGui import init_display
 
-# 制御点グリッド（例：5x5の格子、u方向・v方向ともに閉じる）
+display, start_display, add_menu, add_function_to_menu = init_display()
+
+# 制御点グリッド（例：5x5の格子、u/v方向とも閉じる）
+nx, ny = 10, 11
 control_points = np.array([
-    [(np.cos(2 * np.pi * i / 5) * 2 + np.sin(2 * np.pi * j / 5), np.sin(2 * np.pi *
-      i / 5) * 2 + np.cos(2 * np.pi * j / 5), np.sin(i + j)) for j in range(5)]
-    for i in range(5)
+    [
+        (
+            np.cos(2 * np.pi * i / nx) * 2 + np.sin(2 * np.pi * j / ny),
+            np.sin(2 * np.pi * i / nx) * 2 + np.cos(2 * np.pi * j / ny),
+            np.sin(np.pi * j / ny)
+        ) for i in range(nx)
+    ] for j in range(ny)
 ])
 
 n_u, n_v = control_points.shape[:2]
+x = control_points[:, :, 0]
+y = control_points[:, :, 1]
+z = control_points[:, :, 2]
+
 array = TColgp_Array2OfPnt(1, n_u, 1, n_v)
-for i in range(n_u):
-    for j in range(n_v):
-        x, y, z = control_points[i, j]
-        array.SetValue(i + 1, j + 1, gp_Pnt(x, y, z))
+for ix in range(n_u):
+    for iy in range(n_v):
+        xv, yv, zv = control_points[ix, iy]
+        array.SetValue(ix + 1, iy + 1, gp_Pnt(xv, yv, zv))
+        display.DisplayShape(gp_Pnt(xv, yv, zv))
+
+harray = TColgp_HArray2OfPnt(array)
 
 # BSplineサーフェス生成
-surf_api = GeomAPI_PointsToBSplineSurface(
-    array, True, True, 3, 3)  # u/v方向とも周期的
-surf = surf_api.Surface()
+surf_G1_api = GeomAPI_PointsToBSplineSurface(array, 3, 8, GeomAbs_C0, 0.1E-6)
+surf_G1 = surf_G1_api.Surface()
+
+surf_G2_api = GeomAPI_PointsToBSplineSurface(array, 3, 8, GeomAbs_G2, 0.1E-6)
+surf_G2 = surf_G2_api.Surface()
+
+display.DisplayShape(surf_G1, color='red', transparency=0.5)
+display.DisplayShape(surf_G2, color='blue1', transparency=0.5)
+display.FitAll()
+start_display()
 
 # サーフェスをサンプリングしてプロット
 # uu = np.linspace(surf.FirstUParameter(), surf.LastUParameter(), 50)
@@ -46,20 +72,6 @@ surf = surf_api.Surface()
 # ax.set_title("OCC: Closed BSpline Surface")
 # plt.legend()
 
-import numpy as np
-import matplotlib.pyplot as plt
-from scipy.interpolate import bisplrep, bisplev
-
-# 制御点グリッド（例：5x5の格子、u/v方向とも閉じる）
-control_points = np.array([
-    [(np.cos(2 * np.pi * i / 5) * 2 + np.sin(2 * np.pi * j / 5), np.sin(2 * np.pi *
-      i / 5) * 2 + np.cos(2 * np.pi * j / 5), np.sin(i + j)) for j in range(5)]
-    for i in range(5)
-])
-n_u, n_v = control_points.shape[:2]
-x = control_points[:, :, 0]
-y = control_points[:, :, 1]
-z = control_points[:, :, 2]
 
 # パラメータ
 u = np.linspace(0, 1, n_u)
