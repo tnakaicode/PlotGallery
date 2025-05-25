ee61a74687198020db8fe97c57d4a6a9754f2cd4
tnakaicode <tnakaicode@gmail.com>
2025-05-26 02:04:44 +0900

    step file names

--- occ_gallery/core_load_step_subshape.py
+++ occ_gallery/core_load_step_subshape.py
@@ -14,36 +14,218 @@ from OCC.Core.BRep import BRep_Builder
 from OCC.Core.TopoDS import TopoDS_Compound, TopoDS_Shape
 from OCC.Extend.DataExchange import read_step_file_with_names_colors
 import random
+import os
+from OCC.Core.IFSelect import IFSelect_RetDone
+from OCC.Core.BRepBuilderAPI import BRepBuilderAPI_Transform
+from OCC.Core.XCAFDoc import XCAFDoc_ColorTool
+from OCC.Core.XCAFDoc import XCAFDoc_DocumentTool_ColorTool
+from OCC.Core.TopLoc import TopLoc_Location
 
-app = XCAFApp_Application.GetApplication()
-doc = TDocStd_Document("pythonocc-doc")
-app.NewDocument("pythonocc-doc", doc)
 
-reader = STEPCAFControl_Reader()
-reader.SetColorMode(True)
-reader.SetNameMode(True)
-reader.SetLayerMode(True)
-stepname = pathlib.Path(r"output.step")
+def read_step_file_names(filename: str):
+    """Returns list of tuples (topods_shape, label, color)
+    Use OCAF.
+    """
+    if not os.path.isfile(filename):
+        raise FileNotFoundError(f"{filename} not found.")
+    # the list:
+    output_shapes = {}
+
+    # create an handle to a document
+    doc = TDocStd_Document("pythonocc-doc-step-import")
+
+    # Get root assembly
+    shape_tool = XCAFDoc_DocumentTool.ShapeTool(doc.Main())
+    color_tool = XCAFDoc_DocumentTool.ColorTool(doc.Main())
+    # layer_tool = XCAFDoc_DocumentTool_LayerTool(doc.Main())
+    # mat_tool = XCAFDoc_DocumentTool_MaterialTool(doc.Main())
+
+    step_reader = STEPCAFControl_Reader()
+    step_reader.SetColorMode(True)
+    step_reader.SetLayerMode(True)
+    step_reader.SetNameMode(True)
+    step_reader.SetMatMode(True)
+    step_reader.SetGDTMode(True)
+
+    status = step_reader.ReadFile(filename)
+    if status == IFSelect_RetDone:
+        step_reader.Transfer(doc)
+
+    locs = []
+
+    def _get_sub_shapes(lab, loc):
+        l_subss = TDF_LabelSequence()
+        shape_tool.GetSubShapes(lab, l_subss)
+        # print("Nb subshapes   :", l_subss.Length())
+        l_comps = TDF_LabelSequence()
+        shape_tool.GetComponents(lab, l_comps)
+
+        name = lab.GetLabelName()
+        print("Name :", name)
+
+        if shape_tool.IsAssembly(lab):
+            l_c = TDF_LabelSequence()
+            shape_tool.GetComponents(lab, l_c)
+            for i in range(l_c.Length()):
+                label = l_c.Value(i + 1)
+                if shape_tool.IsReference(label):
+                    # print("\n########  reference label :", label)
+                    label_reference = TDF_Label()
+                    shape_tool.GetReferredShape(label, label_reference)
+                    loc = shape_tool.GetLocation(label)
+                    locs.append(loc)
+                    _get_sub_shapes(label_reference, loc)
+                    locs.pop()
+
+        elif shape_tool.IsSimpleShape(lab):
+            # print("\n########  simpleshape label :", lab)
+            shape = shape_tool.GetShape(lab)
+            # print("    all ass locs   :", locs)
+
+            loc = TopLoc_Location()
+            for l in locs:
+                loc = loc.Multiplied(l)
+
+            c = Quantity_Color(0.5, 0.5, 0.5, Quantity_TOC_RGB)  # default color
+            color_set = False
+            if (
+                color_tool.GetInstanceColor(shape, 0, c)
+                or color_tool.GetInstanceColor(shape, 1, c)
+                or color_tool.GetInstanceColor(shape, 2, c)
+            ):
+                color_tool.SetInstanceColor(shape, 0, c)
+                color_tool.SetInstanceColor(shape, 1, c)
+                color_tool.SetInstanceColor(shape, 2, c)
+                color_set = True
+                n = c.Name(c.Red(), c.Green(), c.Blue())
+                print(
+                    "    instance color Name & RGB: ",
+                    c,
+                    n,
+                    c.Red(),
+                    c.Green(),
+                    c.Blue(),
+                )
+
+            if not color_set:
+                if (
+                    XCAFDoc_ColorTool.GetColor(lab, 0, c)
+                    or XCAFDoc_ColorTool.GetColor(lab, 1, c)
+                    or XCAFDoc_ColorTool.GetColor(lab, 2, c)
+                ):
+                    color_tool.SetInstanceColor(shape, 0, c)
+                    color_tool.SetInstanceColor(shape, 1, c)
+                    color_tool.SetInstanceColor(shape, 2, c)
+
+                    n = c.Name(c.Red(), c.Green(), c.Blue())
+                    print(
+                        "    shape color Name & RGB: ",
+                        c,
+                        n,
+                        c.Red(),
+                        c.Green(),
+                        c.Blue(),
+                    )
+
+            shape_disp = BRepBuilderAPI_Transform(shape, loc.Transformation()).Shape()
+            if shape_disp not in output_shapes:
+                output_shapes[shape_disp] = [lab.GetLabelName(), c]
+            for i in range(l_subss.Length()):
+                lab_subs = l_subss.Value(i + 1)
+                # print("\n########  simpleshape subshape label :", lab)
+                shape_sub = shape_tool.GetShape(lab_subs)
+
+                c = Quantity_Color(0.5, 0.5, 0.5, Quantity_TOC_RGB)  # default color
+                color_set = False
+                if (
+                    color_tool.GetInstanceColor(shape_sub, 0, c)
+                    or color_tool.GetInstanceColor(shape_sub, 1, c)
+                    or color_tool.GetInstanceColor(shape_sub, 2, c)
+                ):
+                    color_tool.SetInstanceColor(shape_sub, 0, c)
+                    color_tool.SetInstanceColor(shape_sub, 1, c)
+                    color_tool.SetInstanceColor(shape_sub, 2, c)
+                    color_set = True
+                    n = c.Name(c.Red(), c.Green(), c.Blue())
+                    print(
+                        "    instance color Name & RGB: ",
+                        c,
+                        n,
+                        c.Red(),
+                        c.Green(),
+                        c.Blue(),
+                    )
+
+                if not color_set:
+                    if (
+                        XCAFDoc_ColorTool.GetColor(lab_subs, 0, c)
+                        or XCAFDoc_ColorTool.GetColor(lab_subs, 1, c)
+                        or XCAFDoc_ColorTool.GetColor(lab_subs, 2, c)
+                    ):
+                        color_tool.SetInstanceColor(shape, 0, c)
+                        color_tool.SetInstanceColor(shape, 1, c)
+                        color_tool.SetInstanceColor(shape, 2, c)
+
+                        n = c.Name(c.Red(), c.Green(), c.Blue())
+                        print(
+                            "    shape color Name & RGB: ",
+                            c,
+                            n,
+                            c.Red(),
+                            c.Green(),
+                            c.Blue(),
+                        )
+                shape_to_disp = BRepBuilderAPI_Transform(
+                    shape_sub, loc.Transformation()
+                ).Shape()
+                # position the subshape to display
+                if shape_to_disp not in output_shapes:
+                    output_shapes[shape_to_disp] = [lab_subs.GetLabelName(), c]
+
+    def _get_shapes():
+        labels = TDF_LabelSequence()
+        shape_tool.GetFreeShapes(labels)
+        print("Number of shapes at root :", labels.Length())
+        for i in range(labels.Length()):
+            root_item = labels.Value(i + 1)
+            _get_sub_shapes(root_item, None)
+
+    _get_shapes()
+    return output_shapes
+
+
+def read_step_file_subshape(filename):
+    app = XCAFApp_Application.GetApplication()
+    doc = TDocStd_Document("pythonocc-doc")
+    app.NewDocument("pythonocc-doc", doc)
+
+    reader = STEPCAFControl_Reader()
+    reader.SetColorMode(True)
+    reader.SetNameMode(True)
+    reader.SetLayerMode(True)
+    stepname = pathlib.Path(r"output.step")
+
+    status = reader.ReadFile(filename)
+
+    if status:
+        reader.Transfer(doc)
+        print("STEP file loaded successfully.")
+    else:
+        print("Failed to load STEP file.")
+
+    shape_tool = XCAFDoc_DocumentTool.ShapeTool(doc.Main())
+    color_tool = XCAFDoc_DocumentTool.ColorTool(doc.Main())
+
+    root_labels = TDF_LabelSequence()
+    shape_tool.GetFreeShapes(root_labels)
+    print(root_labels.Length())
+    if root_labels.Length() > 0:
+        root_label = root_labels.Value(1)
+        sub_labels = TDF_LabelSequence()
+        shape_tool.GetSubShapes(root_label, sub_labels)
+        print(sub_labels.Length())
+
 
 filename = "./assets/models/as1-oc-214.stp"
-status = reader.ReadFile(filename)
-
-if status:
-    reader.Transfer(doc)
-    print("STEP file loaded successfully.")
-else:
-    print("Failed to load STEP file.")
-
-shape_tool = XCAFDoc_DocumentTool.ShapeTool(doc.Main())
-color_tool = XCAFDoc_DocumentTool.ColorTool(doc.Main())
-
-root_labels = TDF_LabelSequence()
-shape_tool.GetFreeShapes(root_labels)
-print(root_labels.Length())
-if root_labels.Length() > 0:
-    root_label = root_labels.Value(1)
-    sub_labels = TDF_LabelSequence()
-    shape_tool.GetSubShapes(root_label, sub_labels)
-    print(sub_labels.Length())
-
-read_step_file_with_names_colors(filename)
+read_step_file_subshape(filename)
+read_step_file_names(filename)
