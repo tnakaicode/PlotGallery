66e38fdef617602c56a816bf0aa501c3c854dba7
tnakaicode <tnakaicode@gmail.com>
2025-11-29 16:13:47 +0900

    offset make solid

--- occ_gallery/core_offset_surface.py
+++ occ_gallery/core_offset_surface.py
@@ -12,6 +12,10 @@ from OCC.Core.TColgp import TColgp_Array2OfPnt
 from OCC.Core.gp import gp_Pnt
 from OCC.Core.BRepBuilderAPI import BRepBuilderAPI_MakeFace
 from OCC.Display.SimpleGui import init_display
+from OCC.Core.TopExp import TopExp_Explorer
+from OCC.Core.TopAbs import TopAbs_WIRE, TopAbs_SHELL
+from OCC.Core.BRepOffsetAPI import BRepOffsetAPI_ThruSections
+from OCC.Core.BRepBuilderAPI import BRepBuilderAPI_Sewing, BRepBuilderAPI_MakeSolid
 
 
 def make_bspline_surface(nx=6, ny=6):
@@ -21,7 +25,7 @@ def make_bspline_surface(nx=6, ny=6):
         u = (i - 1) / (nx - 1)
         for j in range(1, ny + 1):
             v = (j - 1) / (ny - 1)
-            z = 0.15 * math.sin(math.pi * u * 0.9) * math.sin(math.pi * v * 0.8)
+            z = 0.15 * math.sin(math.pi * u * 1.2) * math.sin(math.pi * v * 1.5)
             t.SetValue(i, j, gp_Pnt(u, v, z))
     builder = GeomAPI_PointsToBSplineSurface(t)
     surf = builder.Surface()
@@ -44,6 +48,57 @@ def make_face_from_surface(surf):
     return None
 
 
+def build_solid_from_faces(face0, face1, tol=1e-6):
+    """Given two TopoDS_Face objects (original and offset), build a connecting
+    loft shell, sew faces and shell, and return (solid, loft_shell).
+    Returns (None, None) if construction cannot be completed.
+    """
+    exp0 = TopExp_Explorer(face0, TopAbs_WIRE)
+    wires0 = []
+    while exp0.More():
+        wires0.append(exp0.Current())
+        exp0.Next()
+
+    exp1 = TopExp_Explorer(face1, TopAbs_WIRE)
+    wires1 = []
+    while exp1.More():
+        wires1.append(exp1.Current())
+        exp1.Next()
+
+    if not wires0 or not wires1:
+        return None, None
+
+    w0 = wires0[0]
+    w1 = wires1[0]
+
+    thru = BRepOffsetAPI_ThruSections(True, True, tol)
+    thru.AddWire(w0)
+    thru.AddWire(w1)
+    thru.Build()
+    loft_shell = thru.Shape()
+
+    sewing = BRepBuilderAPI_Sewing(tol)
+    sewing.Add(face0)
+    sewing.Add(face1)
+    sewing.Add(loft_shell)
+    sewing.Perform()
+    sewed = sewing.SewedShape()
+
+    exp_shell = TopExp_Explorer(sewed, TopAbs_SHELL)
+    shell_found = None
+    while exp_shell.More():
+        shell_found = exp_shell.Current()
+        break
+
+    shell_to_use = shell_found if shell_found is not None else sewed
+
+    maker = BRepBuilderAPI_MakeSolid()
+    maker.Add(shell_to_use)
+    solid = maker.Solid()
+
+    return solid, loft_shell
+
+
 if __name__ == "__main__":
     display, start_display, add_menu, add_function_to_menu = init_display()
 
@@ -57,9 +112,15 @@ if __name__ == "__main__":
     face0 = make_face_from_surface(surf)
     face1 = make_face_from_surface(off_surf)
 
-    # display original surface (blue) and offset surface (green)
+    solid, loft_shell = build_solid_from_faces(face0, face1)
+
+    # display original surface (blue), offset surface (green), loft (yellow) and solid (red)
     display.DisplayShape(face0, update=True, transparency=0.5, color="BLUE1")
     display.DisplayShape(face1, update=True, transparency=0.5, color="GREEN")
+    if loft_shell is not None:
+        display.DisplayShape(loft_shell, update=True, transparency=0.6, color="YELLOW")
+    if solid is not None:
+        display.DisplayShape(solid, update=True, transparency=0.0, color="RED")
 
     display.FitAll()
     start_display()
